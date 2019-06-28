a529f76e0b3b155af891ed52607dda64f0cca30b
Alexey Stratulat <alexey.stratulat@demlabs.net>
2019-06-28 18:34:50 +0700

    I brought the file with the main_test test in order. Now main_test.py returns the final code, creates a configuration file as in libdap, so that the test passes, replaces all print with logIt.

--- test/main_test.py
+++ test/main_test.py
@@ -1,24 +1,46 @@
 import libdapConnector
+import os
+import sys
+
+def output_info(string):
+    libdapConnector.logIt(libdapConnector.LogLevel.L_INFO, string)
+def output_info_failed(string):
+    libdapConnector.logIt(libdapConnector.LogLevel.L_CRITICAL, string)
+def create_config_file(app_name):
+    f = open(app_name+".cfg", "w")
+    f.write("[server]\nlisten_address=0.0.0.0\n")
+    f.close()
+
+output_info("Start main test")
+app_name = "TestAPP"
+output_info("Create config file")
+create_config_file(app_name)
+
 
 json_string = """{
     "modules": "libdap",
     "dap": {
-       "config_dir": "/opt/dap/etc",
+       "config_dir": \""""+os.getcwd()+"""\",
        "log_level": \""""+libdapConnector.LogLevel.L_DEBUG.name+"""\",
-       "application_name": "TestAPP"
+       "application_name": \""""+app_name+"""\"
     }
     }"""
-print("Standard Configuration \n"+json_string)
 libdapConnector.init(json_string)
-print("Initialization of the DAP")
+output_info("Initialization of the DAP done")
 libdapConnector.setLogLevel(libdapConnector.LogLevel.L_DEBUG)
-print("Level logging ""DEBUG"" done")
+output_info("Level logging ""DEBUG"" done")
 libdapConnector.logIt(libdapConnector.LogLevel.L_DEBUG, "Test. Outputting a string using the log_it function in the libdap library")
-print("Outputting a string using the log_it function done")
+output_info("Outputting a string using the log_it function done")
 res1 = libdapConnector.configGetItem("server", "listen_address")
-print("Output [server] 'listen_address' = "+res1+"\n")
+output_info("Output [server] 'listen_address' = "+res1+"\n")
 res2 = libdapConnector.configGetItemDefault("server1", "listen_address", "8.8.8.8")
-print("Output default value '8.8.8.8' \n [server1] 'listen_address' = "+res2+"\n")
-print("TEST. Get default config done")
+output_info("Output default value '8.8.8.8' \n [server1] 'listen_address' = "+res2+"\n")
+output_info("TEST. Get default config done")
 libdapConnector.deInit()
-print("Deinitialization done")
+output_info("Deinitialization done")
+
+os.remove(app_name+".cfg")
+output_info("Dellete config file")
+output_info("Main test done");
+
+sys.exit(0)
