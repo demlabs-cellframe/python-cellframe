7358091f29adcf91774d6031c591291a07ec4a6e
Alexey Stratulat <alexey.stratulat@demlabs.net>
2019-07-04 21:15:19 +0700

    [*] Changed, hiring, that everything would be done in the same style.

--- test/test.py
+++ test/test.py
@@ -8,13 +8,13 @@ class TestLibdapCryptoPythonModule(unittest.TestCase):
         self.assertTrue(crypto.deinit() == 0, "Failed deinit libdap crypto python")
     def test_b58(self):
         s = "REvgshguqwt76thuioh1`lwi0-8i-d0hjwpeocnpgh89efty7ug"
-        crypt = crypto.encode_base58(s)
-        decrypt = crypto.decode_base58(crypt)
+        crypt = crypto.encodeBase58(s)
+        decrypt = crypto.decodeBase58(crypt)
         self.assertTrue(s == decrypt, "Encoding and decoded information using the base58 algorithm does not match the original")
     def test_b64(self):
         s = "LindyfekrngFHJFGR23356fer"
-        crypt = crypto.encode_base64(s)
-        decrypt = crypto.decode_base64(crypt, len(s))
+        crypt = crypto.encodeBase64(s)
+        decrypt = crypto.decodeBase64(crypt, len(s))
         self.assertTrue(s == decrypt, "Encoding and decoded information using the base64 algorithm does not match the original")
 
 if __name__ == '__main__':
