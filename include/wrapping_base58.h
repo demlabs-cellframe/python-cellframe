e84d5227b056c240200d6428c41aa7d81bf11c53
Alexey Stratulat <alexey.stratulat@demlabs.net>
2019-07-23 17:49:31 +0700

    [*] Added conditions for compiling compiler with ++ code as with

--- include/wrapping_base58.h
+++ include/wrapping_base58.h
@@ -2,8 +2,14 @@
 #include "dap_enc_base58.h"
 #include "dap_common.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 PyObject *dap_encode_base58_py(PyObject *self, PyObject *args);
 
 PyObject *dap_decode_base58_py(PyObject *self, PyObject *args);
 
-void* getData(PyObject* obj);
+#ifdef __cplusplus
+}
+#endif
