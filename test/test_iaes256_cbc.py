bac460fa733e448034906106d3923b98ed48a0fc
alexey.stratulat <alexey.stratulat@demlabs.net>
2019-09-24 22:28:53 +0700

    [*] The algorithm  IAES 256CBC in unit test has been fixed.

--- test/test_iaes256_cbc.py
+++ test/test_iaes256_cbc.py
@@ -1,21 +1,17 @@
-import libdap_crypto_python_module as crypto
+import libTPO
 import sys
 
 print ("Start test crypto iaes256 CBC")
 s = "Test! I will crush iaes256!"
-print ("Input data: "+s)
 kex_buff = bytes("123", "utf-8")
 size_kex_buff = len(kex_buff)
 seed = bytes(112771128)
 seed_size = len(seed)
-crypto.init()
-key_id = crypto.generateNewKey(0, kex_buff, size_kex_buff, seed, seed_size, 0)
-print(key_id)
+libTPO.init()
+key_id = libTPO.Crypto.generateNewKey(0, kex_buff, size_kex_buff, seed, seed_size, 0)
 source = bytes(s, "utf-8")
-enc = crypto.encryptIAES256CBCFast(key_id, source, len(source), 2048)
-decrypt = crypto.decryptIAES256CBCFast(key_id, enc, len(enc), 2048)
-print (decrypt)
-
+enc = libTPO.Crypto.encryptIAES256CBCFast(key_id, source, len(source), 2048)
+decrypt = libTPO.Crypto.decryptIAES256CBCFast(key_id, enc, len(enc), 2048)
 
 if bytes(s, "utf-8") == decrypt:
     print ("TEST 1. Encode/Decode IAES256 CBC FAST done")
