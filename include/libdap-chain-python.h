a5864bd691c813edd2acc94adb08f3cbdaedc13b
alexey.stratulat <alexey.stratulat@demlabs.net>
2019-10-09 17:48:22 +0700

    [*] Added includes and define at files.

---
+++ include/libdap-chain-python.h
@@ -0,0 +1,11 @@
+#define PY_SSIZE_T_CLEAN
+
+//#define DAP_APP_NAME NODE_NETNAME "-node"
+//#define SYSTEM_PREFIX "/opt/"DAP_APP_NAME
+//#define SYSTEM_CONFIGS_DIR SYSTEM_PREFIX"/etc"
+
+#include "Python.h"
+#include "dap_defines.h"
+//#define DAP_APP_NAME "BINDING_CHAIN_PYTHON"
+#include "dap_chain_node_client.h"
+
---
+++ src/libdap-chain-python.c
@@ -0,0 +1,1 @@
+#include "libdap-chain-python.h"
