5774d9abe90998c74989072d4458f8fe23007141
alexey.stratulat <alexey.stratulat@demlabs.net>
2019-10-10 14:54:37 +0700

    [+] Added wrapping dap_chain_atim_iter_t.

---
+++ include/libdap_chain_atom_iter_python.h
@@ -0,0 +1,62 @@
+#include "Python.h"
+#include "dap_chain.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct PyChainAtomPtr{
+    PyObject_HEAD
+    dap_chain_atom_iter_t *atom_iter;
+} PyChainAtomIterObject;
+
+static PyMethodDef DapChainAtomIterMethods[] = {
+    {NULL, NULL, 0, NULL}
+};
+
+static PyTypeObject dapChainAtomPtr_dapChainAtomPtrType = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+    "CellFrame.ChainAtomIterObject",     /* tp_name */
+    sizeof(PyChainAtomIterObject),       /* tp_basicsize */
+    0,                                  /* tp_itemsize */
+    0,                                  /* tp_dealloc */
+    0,                                  /* tp_print */
+    0,                                  /* tp_getattr */
+    0,                                  /* tp_setattr */
+    0,                                  /* tp_reserved */
+    0,                                  /* tp_repr */
+    0,                                  /* tp_as_number */
+    0,                                  /* tp_as_sequence */
+    0,                                  /* tp_as_mapping */
+    0,                                  /* tp_hash  */
+    0,                                  /* tp_call */
+    0,                                  /* tp_str */
+    0,                                  /* tp_getattro */
+    0,                                  /* tp_setattro */
+    0,                                  /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT |
+        Py_TPFLAGS_BASETYPE,            /* tp_flags */
+    "Chain atom iter objects",           /* tp_doc */
+    0,		                            /* tp_traverse */
+    0,		                            /* tp_clear */
+    0,		                            /* tp_richcompare */
+    0,		                            /* tp_weaklistoffset */
+    0,		                            /* tp_iter */
+    0,		                            /* tp_iternext */
+    DapChainAtomIterMethods,             /* tp_methods */
+    0,                                  /* tp_members */
+    0,                                  /* tp_getset */
+    0,                                  /* tp_base */
+    0,                                  /* tp_dict */
+    0,                                  /* tp_descr_get */
+    0,                                  /* tp_descr_set */
+    0,                                  /* tp_dictoffset */
+    0,                                  /* tp_init */
+    0,                                  /* tp_alloc */
+    PyType_GenericNew,                  /* tp_new */
+
+};
+
+#ifdef __cplusplus
+}
+#endif
