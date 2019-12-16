c009595904e4165a3ac1386e5f5f8a4686f7bf87
alexey.stratulat <alexey.stratulat@demlabs.net>
2019-12-16 21:47:29 +0700

    [+] Added wrapping function dap_chain_net_srv_price_unit_uid_t

--- include/wrapping_dap_chain_common.h
+++ include/wrapping_dap_chain_common.h
@@ -16,7 +16,7 @@ typedef struct PyDapChainHashSlow{
 
 PyObject *dap_chain_hash_slow_to_str_py(PyObject *self, PyObject *args);
 
-PyMethodDef DapChainHashSlowMethod[] = {
+static PyMethodDef DapChainHashSlowMethod[] = {
     {"toStr", (PyCFunction)dap_chain_hash_slow_to_str_py, METH_VARARGS | METH_STATIC, ""},
     {NULL, NULL, 0, NULL}
 };
@@ -77,7 +77,7 @@ PyObject *dap_chain_addr_from_str_py(PyObject *self, PyObject *args);
 PyObject *dap_chain_addr_fill_py(PyObject *self, PyObject *args);
 PyObject *dap_chain_addr_check_sum_py(PyObject *self, PyObject *args);
 
-PyMethodDef DapChainAddrMethods[] = {
+static PyMethodDef DapChainAddrMethods[] = {
     {"toStr", (PyCFunction)dap_chain_addr_to_str_py, METH_VARARGS, ""},
     {"fromStr", (PyCFunction)dap_chain_addr_from_str_py, METH_VARARGS | METH_STATIC, ""},
     {"fill", (PyCFunction)dap_chain_addr_fill_py, METH_VARARGS, ""},
@@ -139,7 +139,7 @@ typedef struct PyDapChainNetId{
 
 PyObject *dap_chain_net_id_from_str_py(PyObject *self, PyObject *args);
 
-PyMethodDef DapChainNetIdObjectMethods[] = {
+static PyMethodDef DapChainNetIdObjectMethods[] = {
     {"fromStr", (PyCFunction)dap_chain_net_id_from_str_py, METH_VARARGS | METH_STATIC, ""},
     {NULL, NULL, 0, NULL}
 };
@@ -197,7 +197,7 @@ typedef struct PyDapChainNetSrvUID{
 
 PyObject *dap_chain_net_srv_uid_from_str_py(PyObject *self, PyObject *args);
 
-PyMethodDef DapChainNetSrvUIDObject[] = {
+static PyMethodDef DapChainNetSrvUIDObject[] = {
     {"fromStr", (PyCFunction)dap_chain_net_srv_uid_from_str_py, METH_VARARGS | METH_STATIC, ""},
     {NULL, NULL, 0, NULL}
 };
@@ -244,6 +244,55 @@ static PyTypeObject DapChainNetSrvUIDObject_DapChainNetSrvUIDObjectType = {
     PyType_GenericNew,               /* tp_new */
 };
 
+/*=================*/
+
+/* Chain net srv uid */
+typedef struct PyDapChainNetSrvPriceUnitUID{
+    PyObject_HEAD
+    dap_chain_net_srv_price_unit_uid_t price_unit_uid;
+}PyDapChainNetSrvPriceUnitUIDObject;
+
+static PyTypeObject DapChainNetSrvPriceUnitUIDObject_DapChainNetSrvPriceUnitUIDObjectType = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+    "CellFrame.ChainNetSrvPriceUnitUID",             /* tp_name */
+    sizeof(PyDapChainNetSrvPriceUnitUIDObject),      /* tp_basicsize */
+    0,                                               /* tp_itemsize */
+    0,                                               /* tp_dealloc */
+    0,                                               /* tp_print */
+    0,                                               /* tp_getattr */
+    0,                                               /* tp_setattr */
+    0,                                               /* tp_reserved */
+    0,                                               /* tp_repr */
+    0,                                               /* tp_as_number */
+    0,                                               /* tp_as_sequence */
+    0,                                               /* tp_as_mapping */
+    0,                                               /* tp_hash  */
+    0,                                               /* tp_call */
+    0,                                               /* tp_str */
+    0,                                               /* tp_getattro */
+    0,                                               /* tp_setattro */
+    0,                                               /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT |
+        Py_TPFLAGS_BASETYPE,                         /* tp_flags */
+    "Chain net srv price unit uid object",           /* tp_doc */
+    0,		                                         /* tp_traverse */
+    0,		                                         /* tp_clear */
+    0,		                                         /* tp_richcompare */
+    0,                                               /* tp_weaklistoffset */
+    0,		                                         /* tp_iter */
+    0,		                                         /* tp_iternext */
+    0,                                               /* tp_methods */
+    0,                                               /* tp_members */
+    0,                                               /* tp_getset */
+    0,                                               /* tp_base */
+    0,                                               /* tp_dict */
+    0,                                               /* tp_descr_get */
+    0,                                               /* tp_descr_set */
+    0,                                               /* tp_dictoffset */
+    0,                                               /* tp_init */
+    0,                                               /* tp_alloc */
+    PyType_GenericNew,                               /* tp_new */
+};
 /*=================*/
 
 
