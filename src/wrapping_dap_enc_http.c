03a65e766326d4c43f055d07a57ec86e5bd443db
alexey.stratulat <alexey.stratulat@demlabs.net>
2019-12-18 16:01:08 +0700

    [+] Added object EncHttp and wrapping function enc_http_add_proc

---
+++ src/wrapping_dap_enc_http.c
@@ -0,0 +1,10 @@
+#include "wrapping_dap_enc_http.h"
+
+PyObject *enc_http_add_proc_py(PyObject *self, PyObject *args){
+    PyObject *obj_server;
+    const char *HTTP_URL;
+    if (!PyArg_ParseTuple(args, "O|s", &obj_server, &HTTP_URL))
+        return NULL;
+    enc_http_add_proc_py(((PyDapServerObject*)obj_server)->t_server, HTTP_URL);
+    return PyLong_FromLong(0);
+}
