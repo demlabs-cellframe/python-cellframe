6fc137f6d1bef827d3102648f1eb9fe3b64d731a
alexey.stratulat <alexey.stratulat@demlabs.net>
2020-01-09 15:42:45 +0700

    [*] Renamed dir_cfg to tmp_cfg. Deleted sections cdb, cdb_auth, srv_vpn from JSON. Added reading adress and port from JSON for listen.

--- test/example_node.py
+++ test/example_node.py
@@ -2,14 +2,14 @@ from CellFrame import *
 import os
 
 app_name = "example-node"
-dir_cfg = os.getcwd() + "/testdir"
+tmp_dir = os.getcwd() + "/testdir"
 
 json_string = """{
     "modules": ["Crypto", "ServerCore", "Http", "HttpFolder", "GlobalDB", "Client", "HttpClientSimple", "Mempool",
      "Chain", "Wallet", "ChainCSDag", "ChainCSDagPoa", "ChainCSDagPos", "GDB", "Net", "ChainNetSrv", "EncHttp",
      "Stream", "StreamCtl", "HttpSimple", "StreamChChain", "StreamChChainNet", "StreamChChainNetSrv"],
     "DAP": {
-       "config_dir": \""""+dir_cfg+"""\",
+       "config_dir": \""""+tmp_dir+"""\",
        "log_level": "L_DEBUG",
        "application_name": \""""+app_name+"""\",
        "file_name_log": \""""+app_name+""".text\"
@@ -28,40 +28,20 @@ json_string = """{
         "mempool": {
              "accept": false
         },
-        "cdb": {
-            "enabled": false,
-            "db_path": "mongodb://localhost/db",
-            "servers_list_enabled": false,
-            "servers_list_networks": ["kelvin-testnet", "private"]
-        },
-        "cdb_auth": {
-            "enabled": false,
-            "collection_name": "mycollection",
-            "domain": "mydomain",
-            "tx_cond_create": false
-        },
-        "srv_vpn": {
-            "enabled": false,
-            "network_address": "10.11.12.0",
-            "network_mask": "255.255.255.0",
-            "pricelist": [
-                "kelvin-testnet:0.00001:KELT:3600:SEC:mywallet0", "kelvin-testnet:0.00001:cETH:3600:SEC:mywallet1", "private:1:WOOD:10:SEC:mywallet0"
-            ]
-         },
         "conserver": {
             "enabled": true,
-            "listen_unix_socket_path": \""""+dir_cfg+"""/run/node_cli\"
+            "listen_unix_socket_path": \""""+tmp_dir+"""/run/node_cli\"
         },
         "resources": {
             "threads_cnt": 0,
-            "pid_path": \""""+dir_cfg+"""/run/cellframe-node.pid\",
-            "log_file": \""""+dir_cfg+"""/log/cellframe-node.log\",
-            "wallets_path": \""""+dir_cfg+"""/lib/wallet\",
+            "pid_path": \""""+tmp_dir+"""/run/cellframe-node.pid\",
+            "log_file": \""""+tmp_dir+"""/log/cellframe-node.log\",
+            "wallets_path": \""""+tmp_dir+"""/lib/wallet\",
             "ca_folders": [
-                \""""+dir_cfg+"""/lib/ca\",
-                \""""+dir_cfg+"""/share/ca\"
+                \""""+tmp_dir+"""/lib/ca\",
+                \""""+tmp_dir+"""/share/ca\"
             ],
-            "dap_global_db_path": \""""+dir_cfg+"""/lib/global_db\",
+            "dap_global_db_path": \""""+tmp_dir+"""/lib/global_db\",
             "dap_global_db_driver": "cdb"
         },
         "networks":{
@@ -108,6 +88,8 @@ json_string = """{
 init(json_string)
 setLogLevel(DEBUG)
 
+server_host_name = configGetItem("server", "listen_address")
+server_port = int(configGetItem("server", "listen_port_tcp"))
 sr = ServerCore.listen("0.0.0.0", 3307, 0)
 Http.new(sr, app_name)
 EncHttp.addProc(sr, "/enc_http")
