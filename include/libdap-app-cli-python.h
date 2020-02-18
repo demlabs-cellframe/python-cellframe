f2600596e48b33d2b8e140c93341ffbe709f580c
Dmitriy A. Gerasimov <dmitriy.gerasimov@demlabs.net>
2020-02-18 21:15:04 +0700

    [*] Bind fixed

---
+++ include/libdap-app-cli-python.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+#include "dap_common.h"
+#include "dap_app_cli.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+typedef struct PyAppCli{
+    PyObject_HEAD
+}PyAppCliObject;
+
+
+PyObject* dap_app_cli_main_py(PyObject *self, PyObject *args);
+
+static PyMethodDef AppCliMethods[] = {
+        {"main", dap_app_cli_main_py, METH_VARARGS | METH_STATIC, "Main CLI function"},
+        {NULL, NULL, 0, NULL}
+};
+
+static PyTypeObject DapAppCli_dapAppCliType = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+    "CellFrame.AppCli",             /* tp_name */
+    sizeof(PyAppCliObject),         /* tp_basicsize */
+    0,                         /* tp_itemsize */
+    0,                         /* tp_dealloc */
+    0,                         /* tp_print */
+    0,                         /* tp_getattr */
+    0,                         /* tp_setattr */
+    0,                         /* tp_reserved */
+    0,                         /* tp_repr */
+    0,                         /* tp_as_number */
+    0,                         /* tp_as_sequence */
+    0,                         /* tp_as_mapping */
+    0,                         /* tp_hash  */
+    0,                         /* tp_call */
+    0,                         /* tp_str */
+    0,                         /* tp_getattro */
+    0,                         /* tp_setattro */
+    0,                         /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT |
+        Py_TPFLAGS_BASETYPE,   /* tp_flags */
+    "AppCli object",           /* tp_doc */
+    0,		               /* tp_traverse */
+    0,		               /* tp_clear */
+    0,		               /* tp_richcompare */
+    0,		               /* tp_weaklistoffset */
+    0,		               /* tp_iter */
+    0,		               /* tp_iternext */
+    AppCliMethods,             /* tp_methods */
+    0,                         /* tp_members */
+    0,                         /* tp_getset */
+    0,                         /* tp_base */
+    0,                         /* tp_dict */
+    0,                         /* tp_descr_get */
+    0,                         /* tp_descr_set */
+    0,                         /* tp_dictoffset */
+    0,                         /* tp_init */
+    0,                         /* tp_alloc */
+    PyType_GenericNew,         /* tp_new */
+    0,                         /* tp_free */
+    0,                         /* tp_is_gc*/
+    0,                          /* tp_bases*/
+    0,                           /* tp_mro */
+    0,                           /* tp_cache */
+    0,                           /* tp_subclasses */
+    0,                           /* tp_weaklist */
+    0,                           /* tp_del */
+    0,                          /* tp_version_tag*/
+    0,                         /* tp_finalize*/
+};
+
+#ifdef  __cplusplus
+}
+#endif
---
+++ src/libdap-app-cli-python.c
@@ -0,0 +1,32 @@
+#include "dap_common.h"
+#include "libdap-app-cli-python.h"
+
+#define LOG_TAG "libdap-app-cli-crypto"
+
+/**
+ * @brief dap_app_cli_main_py
+ * @param self
+ * @param args
+ * @return
+ */
+PyObject* dap_app_cli_main_py(PyObject *self, PyObject *args)
+{
+    (void) self;
+    char *l_app_name = NULL;
+    int l_argc = 0;
+    char ** l_argv = NULL;
+    PyObject* l_args_py = NULL;
+    if (!PyArg_ParseTuple(args, "sI", &l_app_name, &l_args_py))
+            return NULL;
+
+    Py_ssize_t l_args_py_size = PyList_Size(l_args_py);
+    if ( l_args_py_size ){
+        l_argc = l_args_py_size;
+        l_argv = DAP_NEW_Z_SIZE(char*, sizeof(char**)*l_argc  );
+        for (  int i = 0; i< l_argc; i++){
+            l_argv[i] = PyBytes_AsString(PyList_GetItem(l_args_py, i));
+        }
+    }
+
+    return PyLong_FromLong((long) dap_app_cli_main(l_app_name,l_argc,l_argv));
+}
