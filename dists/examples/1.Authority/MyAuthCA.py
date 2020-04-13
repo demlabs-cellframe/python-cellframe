b8e22ba9e4efd6dd13f8b8660118b61d97bfa903
Dmitriy A. Gerasimov <dmitriy.gerasimov@demlabs.net>
2020-04-14 01:53:08 +0700

    ...

--- dists/examples/1.Authority/MyAuthCA.py
+++ dists/examples/1.Authority/MyAuthCA.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-from CellFrame.libCellFrame import *
+from CellFrame.libCellFrame import init, Cert,CryptoKeyType
 from string import Template
 import os
 import sys
 import MyAuthConf
+import random
+import string
 
+
+# ---- Vars ----
 # App name
 appName = "MyAuth"
 
@@ -18,9 +22,27 @@ except json.decoder.JSONDecodeError as jex:
     sys.stderr.write("load_json_config JSONdecode :%s" % jex)
     exit(-1)
 
-# Prepare to parse input arguments
-counter=0
 
+
+def randomString(stringLength=10):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+# Action Help
+def help():
+    print("CA managment script usage: ")
+    print("")
+    print("To get this help")
+    print("\t"+cmdName+" [help]")
+    print("")
+    print("Generate <Root nodes number> certificates (5 by default)" +
+          "for selected <Algorythm> (\"sig_dil\" by default)")
+    print("\t"+cmdName+" init_root_ca [<Root nodes number>] [<Algorythm>] [<Restore string>]")
+    print("")
+
+
+counter = 0
 action = "help"
 cmdName = ""
 action_arg = {}
@@ -36,19 +58,8 @@ for arg in sys.argv:
     if counter == 2:
         action = arg
     elif counter > 2:
-        action_arg[counter-2]=arg
+        action_arg[counter-2] = arg
 # Process actions
-# Action Help
-def help():
-    print("CA managment script usage: ")
-    print("")
-    print("To get this help")
-    print("\t"+cmdName+" [help]")
-    print("")
-    print("Generate <Root nodes number> certificates (5 by default)" +
-          "for selected <Algorythm> (\"sig_dil\" by default)")
-    print("\t"+cmdName+" init_root_ca [<Root nodes number>] [<Algorythm>]")
-    print("")
 if action == "help":
     help()
 # Action init root CAs
@@ -56,26 +67,28 @@ elif action == "init_root_ca":
     # Default params
     rootCaNumber = 5
     rootCaAlgoName = "sig_dil"
+    restoreString = randomString(12)
 
     # Set default algo Dilithium
-    rootCaAlgo = CryptoKeyType.DAP_ENC_KEY_TYPE_SIG_DILITHIUM
+    rootCaAlgo = CryptoKeyType.DAP_ENC_KEY_TYPE_SIG_DILITHIUM()
 
     # Read action args
     if len(action_arg) > 0:
         rootCaNumber = action_arg[1]
     if len(action_arg) > 1:
         rootCaAlgoName = action_arg[2]
-
+    if len(action_arg) > 2:
+        restoreString = action_arg[3]
 
     # Parse algo name
     if rootCaAlgoName == "sig_bliss":
-        rootCaAlgo = CryptoKeyType.DAP_ENC_KEY_TYPE_SIG_BLISS
+        rootCaAlgo = CryptoKeyType.DAP_ENC_KEY_TYPE_SIG_BLISS()
     elif rootCaAlgoName == "sig_tesla":
-        rootCaAlgo = CryptoKeyType.DAP_ENC_KEY_TYPE_SIG_TESLA
+        rootCaAlgo = CryptoKeyType.DAP_ENC_KEY_TYPE_SIG_TESLA()
     elif rootCaAlgoName == "sig_picnic":
-        rootCaAlgo = CryptoKeyType.DAP_ENC_KEY_TYPE_SIG_PICNIC
+        rootCaAlgo = CryptoKeyType.DAP_ENC_KEY_TYPE_SIG_PICNIC()
     elif rootCaAlgoName == "sig_dil":
-        rootCaAlgo = CryptoKeyType.DAP_ENC_KEY_TYPE_SIG_DILITHIUM
+        rootCaAlgo = CryptoKeyType.DAP_ENC_KEY_TYPE_SIG_DILITHIUM()
     else:
         # Process error case
         print("(!) Wrong algo name \""+rootCaAlgoName+"\", possible names: sig_bliss, sig_tesla, sig_picnic, sig_dil")
@@ -83,7 +96,11 @@ elif action == "init_root_ca":
         exit(-1)
 
     # Create certs
-    print("Init root "+str(rootCaNumber)+" certificates with algo "+rootCaAlgoName)
+    print("Init root "+str(rootCaNumber)+" certificates with algo "
+          + rootCaAlgoName)
+    print("Record somewhere the restore string(without braces): \""
+          + restoreString+"\"")
     for cur in range(int(rootCaNumber)):
-        certName = MyAuthConf.getNetworkName(appName,"_") +"_root_"+str(cur)
-        cert=Cert.generate( certName, rootCaAlgo,"fasdfsdf" )
+        cname = MyAuthConf.getNetworkName(appName, "_") + "_root_" + str(cur)
+        cert = Cert.generate(cname, rootCaAlgo, restoreString)
+        cert.save()
