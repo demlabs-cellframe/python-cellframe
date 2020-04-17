d47fc417dc3831eef27d985c38f2f1f3e34a8148
alexey.stratulat <alexey.stratulat@demlabs.net>
2020-04-17 17:41:12 +0700

    [+] Creating based for wrapping libdap-chain-wallet.

---
+++ src/dap_chain_wallet_python.c
@@ -0,0 +1,8 @@
+#include "dap_chain_wallet_python.h"
+
+int dap_chain_wallet_init_py(void){
+    return dap_chain_wallet_init();
+}
+void dap_chain_wallet_deinit_py(void){
+    dap_chain_wallet_deinit();
+}
