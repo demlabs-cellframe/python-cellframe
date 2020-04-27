e357b221dbddace45498c1ba3861b424a66834c8
Dmitriy A. Gerasimov <dmitriy.gerasimov@demlabs.net>
2020-04-28 02:45:53 +0700

    [-] Removed old trash

--- include/libdap-chain-python.h
+++ include/libdap-chain-python.h
@@ -2,10 +2,6 @@
 
 #define PY_SSIZE_T_CLEAN
 
-//#define DAP_APP_NAME NODE_NETNAME "-node"
-//#define SYSTEM_PREFIX "/opt/"DAP_APP_NAME
-//#define SYSTEM_CONFIGS_DIR SYSTEM_PREFIX"/etc"
-
 #include "Python.h"
 //#define DAP_APP_NAME "BINDING_CHAIN_PYTHON"
 #include "dap_chain.h"
