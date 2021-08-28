e321c6b16cdf2098e22331f5821be2c99621f6a5
alexey.stratulat <alexey.stratulat@demlabs.net>
2021-08-28 17:34:46 +0700

    [*] Fix script.

--- build_egg.sh
+++ build_egg.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-python3 setup.py bdist -d build -u root -g root --formats egg 
+python3 setup_dap.py bdist -d build -u root -g root --formats egg 
+python3 setup_cellframe.py bdist -d build -u root -g root --formats egg
