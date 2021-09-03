716b47c8057c6972b9c454906cfb26c5cc98def3
alexey.stratulat <alexey.stratulat@demlabs.net>
2021-09-03 11:42:11 +0700

    [+] Added description classes type Base58 and Base64.

--- modules/dap-sdk/crypto/include/wrapping_base58.h
+++ modules/dap-sdk/crypto/include/wrapping_base58.h
@@ -31,9 +31,63 @@
 extern "C" {
 #endif
 
+typedef struct DapCryptoBase58Object{
+    PyObject_HEAD
+}PyDapCryptoBase58Object;
+
+
 PyObject *dap_encode_base58_py(PyObject *self, PyObject *args);
 
 PyObject *dap_decode_base58_py(PyObject *self, PyObject *args);
+
+static PyMethodDef g_crypto_base58_methods_py[] = {
+        {"encode", dap_encode_base58_py, METH_VARARGS | METH_STATIC, ""},
+        {"decode", dap_decode_base58_py, METH_VARARGS | METH_STATIC, ""},
+        {NULL, NULL, 0, NULL}
+};
+
+static PyTypeObject g_crypto_base58_type_py = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+    "DAP.Crypto.Base58",             /* tp_name */
+    sizeof(PyDapCryptoBase58Object),         /* tp_basicsize */
+    0,                         /* tp_itemsize */
+    0,                         /* tp_dealloc */
+    0,                         /* tp_print */
+    0,                         /* tp_getattr */
+    0,                         /* tp_setattr */
+    0,                         /* tp_reserved */
+    0,                         /* tp_repr */
+    0,                         /* tp_as_number */
+    0,                         /* tp_as_sequence */
+    0,                         /* tp_as_mapping */
+    0,                         /* tp_hash  */
+    0,                         /* tp_call */
+    0,                         /* tp_str */
+    0,                         /* tp_getattro */
+    0,                         /* tp_setattro */
+    0,                         /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT |
+        Py_TPFLAGS_BASETYPE,   /* tp_flags */
+    "This class is designed to work with base58 "
+    "decoding encoding algorithms",           /* tp_doc */
+    0,		               /* tp_traverse */
+    0,		               /* tp_clear */
+    0,		               /* tp_richcompare */
+    0,		               /* tp_weaklistoffset */
+    0,		               /* tp_iter */
+    0,		               /* tp_iternext */
+    g_crypto_base58_methods_py,             /* tp_methods */
+    0,                         /* tp_members */
+    0,                         /* tp_getset */
+    0,                         /* tp_base */
+    0,                         /* tp_dict */
+    0,                         /* tp_descr_get */
+    0,                         /* tp_descr_set */
+    0,                         /* tp_dictoffset */
+    0,                         /* tp_init */
+    0,                         /* tp_alloc */
+    PyType_GenericNew,         /* tp_new */
+};
 
 #ifdef __cplusplus
 }
