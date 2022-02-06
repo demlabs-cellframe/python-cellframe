be83bfa2cfb8f90784e792aaa66c2b8aaf59f2fe
alexey.stratulat <alexey.stratulat@demlabs.net>
2022-02-06 23:34:20 +0700

    [+] Added wrapping for struct dap_chain_net_srv_order_direction_t.

--- modules/cellframe-sdk/net/srv/src/wrapping_dap_chain_net_srv_common.c
+++ modules/cellframe-sdk/net/srv/src/wrapping_dap_chain_net_srv_common.c
@@ -15,7 +15,7 @@ PyObject *wrapping_dap_chain_net_srv_get_net(PyObject *self, void *closure){
     PyDapChainNetObject *l_obj_net = PyObject_New(PyDapChainNetObject, &DapChainNetObject_DapChainNetObjectType);
     PyObject_Dir((PyObject*)l_obj_net);
     l_obj_net->chain_net = WRAPPING_DAP_CHAIN_NET_SRV_PRICE(self)->price.net;
-    return l_obj_net;
+    return (PyObject*)l_obj_net;
 }
 PyObject *wrapping_dap_chain_net_srv_get_value_datoshi(PyObject *self, void *closure){
     (void)closure;
@@ -31,8 +31,57 @@ PyObject *wrapping_dap_chain_net_srv_get_units(PyObject *self, void *closure){
 }
 PyObject *wrapping_dap_chain_net_srv_get_units_uid(PyObject *self, void *closure){
     (void)closure;
-    PyDapChainNetSrvPriceUnitUIDObject *l_price_unit_uid = PyObject_New(PyDapChainNetSrvPriceObject, &DapChainNetSrvPriceUnitUIDObject_DapChainNetSrvPriceUnitUIDObjectType);
+    PyDapChainNetSrvPriceUnitUIDObject *l_price_unit_uid = PyObject_New(PyDapChainNetSrvPriceUnitUIDObject, &DapChainNetSrvPriceUnitUIDObject_DapChainNetSrvPriceUnitUIDObjectType);
     PyObject_Dir((PyObject*)l_price_unit_uid);
     l_price_unit_uid->price_unit_uid = WRAPPING_DAP_CHAIN_NET_SRV_PRICE(self)->price.units_uid;
     return (PyObject*)l_price_unit_uid;
+}
+
+/* wrapping dap_chain_net_srv_order_direction */
+
+PyObject *DapChainNetSrvOrderDirection_str(PyObject *self){
+    char *ret;
+    switch (((PyDapChainNetSrvOrderDirectionObject*)self)->direction) {
+        case 1:
+            ret = "SERV_DIR_BUY";
+            break;
+        case 2:
+            ret = "SERV_DIR_SELL";
+            break;
+        default:
+            ret = "SERV_DIR_UNDEFINED";
+            break;
+    }
+    return Py_BuildValue("s", ret);
+}
+
+PyObject *wrapping_dap_chain_net_srv_order_direction_get_serv_dir_buy(PyObject *self, PyObject *args){
+    (void)self;
+    (void)args;
+    PyDapChainNetSrvOrderDirectionObject *l_obj = PyObject_New(
+            PyDapChainNetSrvOrderDirectionObject,
+            &DapChainNetSrvOrderDirectionObject_DapChainNetSrvOrderDirectionObjectType);
+    PyObject_Dir((PyObject*)l_obj);
+    l_obj->direction = SERV_DIR_BUY;
+    return (PyObject*)l_obj;
+}
+PyObject *wrapping_dap_chain_net_srv_order_direction_get_serv_dir_sell(PyObject *self, PyObject *args){
+    (void)self;
+    (void)args;
+    PyDapChainNetSrvOrderDirectionObject *l_obj = PyObject_New(
+            PyDapChainNetSrvOrderDirectionObject,
+            &DapChainNetSrvOrderDirectionObject_DapChainNetSrvOrderDirectionObjectType);
+    PyObject_Dir((PyObject*)l_obj);
+    l_obj->direction = SERV_DIR_SELL;
+    return (PyObject*)l_obj;
+}
+PyObject *wrapping_dap_chain_net_srv_order_direction_get_serv_dir_undefined(PyObject *self, PyObject *args){
+    (void)self;
+    (void)args;
+    PyDapChainNetSrvOrderDirectionObject *l_obj = PyObject_New(
+            PyDapChainNetSrvOrderDirectionObject,
+            &DapChainNetSrvOrderDirectionObject_DapChainNetSrvOrderDirectionObjectType);
+    PyObject_Dir((PyObject*)l_obj);
+    l_obj->direction = SERV_DIR_UNDEFINED;
+    return (PyObject*)l_obj;
 }
