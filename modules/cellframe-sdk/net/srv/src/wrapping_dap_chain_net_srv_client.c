93a0561b9fba5ce13bdba0266cebbb56a66cafd3
alexey.stratulat <alexey.stratulat@demlabs.net>
2022-02-09 19:50:08 +0700

    [+] Fixed callbacks.

--- modules/cellframe-sdk/net/srv/src/wrapping_dap_chain_net_srv_client.c
+++ modules/cellframe-sdk/net/srv/src/wrapping_dap_chain_net_srv_client.c
@@ -2,6 +2,10 @@
 
 #define LOG_TAG "wrapping_dap_chain_net_srv_client"
 
+#define _TYPE_CALLBACK_CONNECTED 0
+#define _TYPE_CALLBACK_DISCONNECTED 1
+#define _TYPE_CALLBACK_DELETE 2
+
 void _wrapping_dap_chain_net_srv_client_callback_connected(dap_chain_net_srv_client_t* a_client, void *a_arg){
     if(PyCallable_Check(a_arg)) {
         PyGILState_STATE state = PyGILState_Ensure();
@@ -10,7 +14,7 @@ void _wrapping_dap_chain_net_srv_client_callback_connected(dap_chain_net_srv_cli
                                                             &DapChainNetSrvClientObject_DapChainNetSrvClientObjectType);
         PyObject_Dir((PyObject *) client);
         client->srv_client = a_client;
-        PyObject *l_args = Py_BuildValue("(O)", client);
+        PyObject *l_args = Py_BuildValue("Oi", client, _TYPE_CALLBACK_CONNECTED);
         Py_INCREF(l_args);
         Py_INCREF(l_call);
         PyEval_CallObject(l_call, l_args);
@@ -18,11 +22,47 @@ void _wrapping_dap_chain_net_srv_client_callback_connected(dap_chain_net_srv_cli
         Py_XDECREF(l_call);
         PyGILState_Release(state);
     }else{
-        log_it(L_ERROR, "Can't called handler Python in callbacks");
+        log_it(L_ERROR, "Can't called handler Python in callback connected");
     }
 }
-void _wrapping_dap_chain_net_srv_client_callback_disconnected(dap_chain_net_srv_client_t* a_client, void *a_arg){}
-void _wrapping_dap_chain_net_srv_client_callback_deleted(dap_chain_net_srv_client_t* a_client, void *a_arg){}
+void _wrapping_dap_chain_net_srv_client_callback_disconnected(dap_chain_net_srv_client_t* a_client, void *a_arg){
+    if(PyCallable_Check(a_arg)) {
+        PyGILState_STATE state = PyGILState_Ensure();
+        PyObject *l_call = (PyObject*)a_arg;
+        PyDapChainNetSrvClientObject *client = PyObject_New(PyDapChainNetSrvClientObject,
+                                                            &DapChainNetSrvClientObject_DapChainNetSrvClientObjectType);
+        PyObject_Dir((PyObject *) client);
+        client->srv_client = a_client;
+        PyObject *l_args = Py_BuildValue("Oi", client, _TYPE_CALLBACK_DISCONNECTED);
+        Py_INCREF(l_args);
+        Py_INCREF(l_call);
+        PyEval_CallObject(l_call, l_args);
+        Py_XDECREF(l_args);
+        Py_XDECREF(l_call);
+        PyGILState_Release(state);
+    }else{
+        log_it(L_ERROR, "Can't called handler Python in callback disconnected");
+    }
+}
+void _wrapping_dap_chain_net_srv_client_callback_deleted(dap_chain_net_srv_client_t* a_client, void *a_arg){
+    if(PyCallable_Check(a_arg)) {
+        PyGILState_STATE state = PyGILState_Ensure();
+        PyObject *l_call = (PyObject*)a_arg;
+        PyDapChainNetSrvClientObject *client = PyObject_New(PyDapChainNetSrvClientObject,
+                                                            &DapChainNetSrvClientObject_DapChainNetSrvClientObjectType);
+        PyObject_Dir((PyObject *) client);
+        client->srv_client = a_client;
+        PyObject *l_args = Py_BuildValue("Oi", client, _TYPE_CALLBACK_DELETE);
+        Py_INCREF(l_args);
+        Py_INCREF(l_call);
+        PyEval_CallObject(l_call, l_args);
+        Py_XDECREF(l_args);
+        Py_XDECREF(l_call);
+        PyGILState_Release(state);
+    }else{
+        log_it(L_ERROR, "Can't called handler Python in callback delete");
+    }
+}
 void _wrapping_dap_chain_net_srv_client_callback_pkt_it(dap_chain_net_srv_client_t* a_client, void *a_arg){}
 
 int PyDapChainNetSrvClient_init(PyDapChainNetSrvClientObject* self, PyObject *args, PyObject *kwds){
@@ -46,7 +86,7 @@ int PyDapChainNetSrvClient_init(PyDapChainNetSrvClientObject* self, PyObject *ar
     callbacks.connected = _wrapping_dap_chain_net_srv_client_callback_connected;
     callbacks.disconnected = _wrapping_dap_chain_net_srv_client_callback_disconnected;
     callbacks.deleted = _wrapping_dap_chain_net_srv_client_callback_deleted;
-//    callbacks.pkt_in = _w
+//    callbacks.
     dap_chain_net_srv_client_create_n_connect(((PyDapChainNetObject*)obj_net)->chain_net, addr, port, &callbacks, obj_hander);
 }
 PyObject *wrapping_dap_chain_net_srv_client_write(PyObject *self, PyObject *args){
