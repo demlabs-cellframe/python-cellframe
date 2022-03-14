07ade4f0d0cabd9ccebd4d3dd8110471130bf6f4
alexey.stratulat <alexey.stratulat@demlabs.net>
2022-03-14 23:07:05 +0700

    [*] Changed statics to globals in cellframe-sdk/app-cli and cellframe-sdk/chain module.

--- modules/cellframe-sdk/chain/include/libdap-chain-python.h
+++ modules/cellframe-sdk/chain/include/libdap-chain-python.h
@@ -42,70 +42,10 @@ PyObject *dap_chain_python_atom_get_datums(PyObject *self, PyObject *args);
 PyObject *dap_chain_python_atom_iter_get_next(PyObject *self, PyObject *args);
 PyObject *dap_chain_python_atom_iter_get_dag(PyObject *self, PyObject *args);
 
-static PyMethodDef DapChainMethods[] = {
-    {"findById", (PyCFunction)dap_chain_find_by_id_py, METH_VARARGS|METH_STATIC, ""},
-    {"loadFromCfg", (PyCFunction)dap_chain_load_from_cfg_py, METH_VARARGS|METH_STATIC, ""},
-    {"hasFileStore", (PyCFunction)dap_chain_has_file_store_py, METH_NOARGS, ""},
-    {"saveAll", (PyCFunction) dap_chain_save_all_py, METH_NOARGS, ""},
-    {"loadAll", (PyCFunction)dap_chain_load_all_py, METH_NOARGS, ""},
-    {"createAtomItem", (PyCFunction) dap_chain_python_create_atom_iter, METH_VARARGS, ""},
-    {"atomIterGetFirst", (PyCFunction) dap_chain_python_atom_iter_get_first, METH_VARARGS, ""},
-    {"atomGetDatums", (PyCFunction) dap_chain_python_atom_get_datums, METH_VARARGS, ""},
-    {"atomIterGetNext", (PyCFunction)dap_chain_python_atom_iter_get_next, METH_VARARGS, ""},
-    {"getDag", (PyCFunction)dap_chain_python_atom_iter_get_dag, METH_NOARGS},
-    //{"close", (PyCFunction)dap_chain_close_py, METH_NOARGS, ""},
-    {NULL, NULL, 0, NULL}
-};
-
-static PyTypeObject dapChainObject_dapChainType = {
-    PyVarObject_HEAD_INIT(NULL, 0)
-    "CellFrame.Chain",                                            /* tp_name */
-    sizeof(PyDapChainObject),                                     /* tp_basicsize */
-    0,                                                            /* tp_itemsize */
-    (destructor)PyDapChainObject_dealloc,                         /* tp_dealloc */
-    0,                                                            /* tp_print */
-    0,                                                            /* tp_getattr */
-    0,                                                            /* tp_setattr */
-    0,                                                            /* tp_reserved */
-    0,                                                            /* tp_repr */
-    0,                                                            /* tp_as_number */
-    0,                                                            /* tp_as_sequence */
-    0,                                                            /* tp_as_mapping */
-    0,                                                            /* tp_hash  */
-    0,                                                            /* tp_call */
-    0,                                                            /* tp_str */
-    0,                                                            /* tp_getattro */
-    0,                                                            /* tp_setattro */
-    0,                                                            /* tp_as_buffer */
-    Py_TPFLAGS_DEFAULT |
-        Py_TPFLAGS_BASETYPE,                                      /* tp_flags */
-    "Chain objects",                                              /* tp_doc */
-    0,		                                                      /* tp_traverse */
-    0,		                                                      /* tp_clear */
-    0,		                                                      /* tp_richcompare */
-    0,		                                                      /* tp_weaklistoffset */
-    0,		                                                      /* tp_iter */
-    0,		                                                      /* tp_iternext */
-    DapChainMethods,                                              /* tp_methods */
-    0,                                                            /* tp_members */
-    0,                                                            /* tp_getset */
-    0,                                                            /* tp_base */
-    0,                                                            /* tp_dict */
-    0,                                                            /* tp_descr_get */
-    0,                                                            /* tp_descr_set */
-    0,                                                            /* tp_dictoffset */
-    0,                                                            /* tp_init */
-    0,                                                            /* tp_alloc */
-    PyDapChainObject_new,                                            /* tp_new */
-
-};
+extern PyTypeObject dapChainObject_dapChainType;
 
 static inline bool PyDapChain_Check(PyObject* self){
-    if (PyObject_TypeCheck(self, &dapChainObject_dapChainType) == 0){
-        return true;
-    } else {
-        return false;
-    }
+    return PyObject_TypeCheck(self, &dapChainObject_dapChainType);
 }
 
 
