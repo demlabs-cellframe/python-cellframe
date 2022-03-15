bcef751d5d5d68e0d1870ac50e9bc2f7dc19cb02
alexey.stratulat <alexey.stratulat@demlabs.net>
2022-03-15 20:54:26 +0700

    [*] Changed statics to globals in dap-sdk/net/server/json_rpc module.

--- modules/dap-sdk/net/server/json_rpc/src/wrapping_json_rpc_request.c
+++ modules/dap-sdk/net/server/json_rpc/src/wrapping_json_rpc_request.c
@@ -2,6 +2,53 @@
 
 #define LOG_TAG "wrapping_json_rpc_request"
 
+PyMethodDef PyDapJSONRPCRequestMethods[] = {
+        {"registrationHandler", (PyCFunction)dap_json_rpc_request_reg_handler_py, METH_VARARGS | METH_STATIC    , ""},
+        {NULL, NULL, 0, NULL}
+};
+
+PyTypeObject DapJSONRPCRequest_DapJSONRPCRequestType = {
+        PyVarObject_HEAD_INIT(NULL, 0)
+        "CellFrame.JSONRPC.Request",             /* tp_name */
+        sizeof(PyDAPJsonRPCRequestObject),                   /* tp_basicsize */
+        0,                                                 /* tp_itemsize */
+        0,                                                 /* tp_dealloc */
+        0,                                                 /* tp_print */
+        0,                                                 /* tp_getattr */
+        0,                                                 /* tp_setattr */
+        0,                                                 /* tp_reserved */
+        0,                                                 /* tp_repr */
+        0,                                                 /* tp_as_number */
+        0,                                                 /* tp_as_sequence */
+        0,                                                 /* tp_as_mapping */
+        0,                                                 /* tp_hash  */
+        0,                                                 /* tp_call */
+        0,                                                 /* tp_str */
+        0,                                                 /* tp_getattro */
+        0,                                                 /* tp_setattro */
+        0,                                                 /* tp_as_buffer */
+        Py_TPFLAGS_DEFAULT |
+        Py_TPFLAGS_BASETYPE,                           /* tp_flags */
+        "Dap JSON RPC request object",                         /* tp_doc */
+        0,                                               /* tp_traverse */
+        0,                                               /* tp_clear */
+        0,                                               /* tp_richcompare */
+        0,                                               /* tp_weaklistoffset */
+        0,                                               /* tp_iter */
+        0,                                               /* tp_iternext */
+        PyDapJSONRPCRequestMethods,                   /* tp_methods */
+        0,                                                 /* tp_members */
+        0,                          /* tp_getset */
+        0,                                                 /* tp_base */
+        0,                                                 /* tp_dict */
+        0,                                                 /* tp_descr_get */
+        0,                                                 /* tp_descr_set */
+        0,                                                 /* tp_dictoffset */
+        0,                                                 /* tp_init */
+        0,                                                 /* tp_alloc */
+        PyType_GenericNew,                                 /* tp_new */
+};
+
 struct _w_json_rpc_handler *handlers = NULL;
 
 void _w_dap_json_rpc_request_handler(dap_json_rpc_params_t *a_params, dap_json_rpc_response_t *a_response, const char *a_method){
--- modules/dap-sdk/net/server/json_rpc/src/wrapping_json_rpc_response.c
+++ modules/dap-sdk/net/server/json_rpc/src/wrapping_json_rpc_response.c
@@ -2,6 +2,59 @@
 
 #define LOG_TAG "wrapping_json_rpc_response"
 
+PyMethodDef PyDapJSONRPCResponseMethods[] = {
+        {NULL, NULL, 0, NULL}
+};
+
+PyGetSetDef PyDapJSONRPCResponseGetsSets[] = {
+        {"ID", (getter)wrapping_json_rpc_response_get_id, NULL, "ID request", NULL},
+        {"Error", (getter)wrapping_json_rpc_response_get_error, (setter)wrapping_json_rpc_response_set_error, "", NULL },
+        {"Result", (getter)wrapping_json_rpc_response_get_result, (setter)wrapping_json_rpc_response_set_result, "", NULL},
+        {NULL, NULL, NULL, NULL, NULL}
+};
+
+PyTypeObject DapJSONRPCResponse_DapJSONRPCResponseType = {
+        PyVarObject_HEAD_INIT(NULL, 0)
+        "CellFrame.JSONRPC.Response",             /* tp_name */
+        sizeof(PyDapJSONRPCResponseObject),                   /* tp_basicsize */
+        0,                                                 /* tp_itemsize */
+        0,                                                 /* tp_dealloc */
+        0,                                                 /* tp_print */
+        0,                                                 /* tp_getattr */
+        0,                                                 /* tp_setattr */
+        0,                                                 /* tp_reserved */
+        0,                                                 /* tp_repr */
+        0,                                                 /* tp_as_number */
+        0,                                                 /* tp_as_sequence */
+        0,                                                 /* tp_as_mapping */
+        0,                                                 /* tp_hash  */
+        0,                                                 /* tp_call */
+        0,                                                 /* tp_str */
+        0,                                                 /* tp_getattro */
+        0,                                                 /* tp_setattro */
+        0,                                                 /* tp_as_buffer */
+        Py_TPFLAGS_DEFAULT |
+        Py_TPFLAGS_BASETYPE,                           /* tp_flags */
+        "Dap JSON RPC response object",                         /* tp_doc */
+        0,                                               /* tp_traverse */
+        0,                                               /* tp_clear */
+        0,                                               /* tp_richcompare */
+        0,                                               /* tp_weaklistoffset */
+        0,                                               /* tp_iter */
+        0,                                               /* tp_iternext */
+        PyDapJSONRPCResponseMethods,                   /* tp_methods */
+        0,                                                 /* tp_members */
+        PyDapJSONRPCResponseGetsSets,                          /* tp_getset */
+        0,                                                 /* tp_base */
+        0,                                                 /* tp_dict */
+        0,                                                 /* tp_descr_get */
+        0,                                                 /* tp_descr_set */
+        0,                                                 /* tp_dictoffset */
+        0,                                                 /* tp_init */
+        0,                                                 /* tp_alloc */
+        PyType_GenericNew,                                 /* tp_new */
+};
+
 int wrapping_json_rpc_response_set_result(PyObject *self, PyObject *value, void *closure){
     UNUSED(closure);
     dap_json_rpc_response_t *l_resp = ((PyDapJSONRPCResponseObject*)self)->response;
