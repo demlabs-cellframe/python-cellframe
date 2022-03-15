3e59cbff046f63574532940babd4fe8611884ebc
alexey.stratulat <alexey.stratulat@demlabs.net>
2022-03-15 20:37:07 +0700

    [*] Changed statics to globals in dap-sdk/net/core module.

--- modules/dap-sdk/net/core/src/dap_events_python.c
+++ modules/dap-sdk/net/core/src/dap_events_python.c
@@ -1,5 +1,55 @@
 #include "dap_events_python.h"
 
+PyMethodDef PyDapEventsObject_methods[] = {
+        {"start", (PyCFunction)dap_events_start_py, METH_NOARGS, ""},
+        {"wait", (PyCFunction)dap_events_wait_py, METH_NOARGS, ""},
+        {"killSocket", (PyCFunction)dap_events_socket_kill_socket_py, METH_VARARGS, ""},
+        {"removeAndDeleteSocket", (PyCFunction)dap_events_socket_remove_and_delete_py, METH_VARARGS, ""},
+        {NULL, NULL, 0, NULL}  /* Sentinel */
+};
+
+PyTypeObject dapEvents_dapEventsType = {
+        PyVarObject_HEAD_INIT(NULL, 0)
+        "CellFrame.ServerCore.DapEvents",           /* tp_name */
+        sizeof(PyDapEventsObject),		        /* tp_basicsize */
+        0,			                        /* tp_itemsize */
+        (destructor)PyDapEventsObject_dealloc,	/* tp_dealloc */
+        0,                         			/* tp_print */
+        0,                         			/* tp_getattr */
+        0,                         			/* tp_setattr */
+        0,                         			/* tp_reserved */
+        0,                         			/* tp_repr */
+        0,                         			/* tp_as_number */
+        0,                         			/* tp_as_sequence */
+        0,                         			/* tp_as_mapping */
+        0,                         			/* tp_hash  */
+        0,                         			/* tp_call */
+        0,                         			/* tp_str */
+        0,                         			/* tp_getattro */
+        0,                         			/* tp_setattro */
+        0,                         			/* tp_as_buffer */
+        Py_TPFLAGS_DEFAULT |
+        Py_TPFLAGS_BASETYPE,   			/* tp_flags */
+        "DapEvents objects",           		/* tp_doc */
+        0,		               			/* tp_traverse */
+        0,		               			/* tp_clear */
+        0,		               			/* tp_richcompare */
+        0,		               			/* tp_weaklistoffset */
+        0,		               			/* tp_iter */
+        0,		               			/* tp_iternext */
+        PyDapEventsObject_methods,			/* tp_methods */
+        0,						/* tp_members */
+        0,						/* tp_getset */
+        0,                         			/* tp_base */
+        0,                         			/* tp_dict */
+        0,                         			/* tp_descr_get */
+        0,                         			/* tp_descr_set */
+        0,                         			/* tp_dictoffset */
+        0,						/* tp_init */
+        0,                         			/* tp_alloc */
+        PyDapEventsObject_new,	                /* tp_new */
+};
+
 PyObject *PyDapEventsObject_new(PyTypeObject *type_object, PyObject *args, PyObject *kwds){
     PyDapEventsObject *new_EO = (PyDapEventsObject*)PyType_GenericNew(type_object, args, kwds);
     new_EO->t_events = dap_events_new();
--- modules/dap-sdk/net/core/src/dap_events_socket_python.c
+++ modules/dap-sdk/net/core/src/dap_events_socket_python.c
@@ -2,6 +2,60 @@
 
 #define LOG_TAG "dap_events_socket_python"
 
+PyMethodDef PyDapEventsObjectSocket_methods[] = {
+        {"createAfter", (PyCFunction)dap_events_socket_create_after_py, METH_NOARGS, ""},
+        {"setReadable", (PyCFunction)dap_events_socket_set_readable_py, METH_VARARGS, ""},
+        {"setWritable", (PyCFunction)dap_events_socket_set_writable_py, METH_VARARGS, ""},
+        {"write", (PyCFunction)dap_events_socket_write_py, METH_VARARGS, ""},
+        {"writeF", (PyCFunction)dap_events_socket_write_f_py, METH_VARARGS, ""},
+        {"read", (PyCFunction)dap_events_socket_read_py, METH_VARARGS, ""},
+        {"delete", (PyCFunction)dap_events_socket_delete_py, METH_VARARGS, ""},
+        {"shringBufIn", (PyCFunction)dap_events_socket_shrink_buf_in_py, METH_VARARGS, ""},
+        {NULL, NULL, 0, NULL}  /* Sentinel */
+};
+
+PyTypeObject dapEventsSocket_dapEventsSocketType = {
+        PyVarObject_HEAD_INIT(NULL, 0)
+        "CellFrame.ServerCore.DapEventsSocket",             /* tp_name */
+        sizeof(PyDapEventsSocketObject),                   /* tp_basicsize */
+        0,                                                 /* tp_itemsize */
+        0,                                                 /* tp_dealloc */
+        0,                                                 /* tp_print */
+        0,                                                 /* tp_getattr */
+        0,                                                 /* tp_setattr */
+        0,                                                 /* tp_reserved */
+        0,                                                 /* tp_repr */
+        0,                                                 /* tp_as_number */
+        0,                                                 /* tp_as_sequence */
+        0,                                                 /* tp_as_mapping */
+        0,                                                 /* tp_hash  */
+        0,                                                 /* tp_call */
+        0,                                                 /* tp_str */
+        0,                                                 /* tp_getattro */
+        0,                                                 /* tp_setattro */
+        0,                                                 /* tp_as_buffer */
+        Py_TPFLAGS_DEFAULT |
+        Py_TPFLAGS_BASETYPE,                           /* tp_flags */
+        "DapEventsSocket objects",                         /* tp_doc */
+        0,		                                       /* tp_traverse */
+        0,                        		               /* tp_clear */
+        0,		                                       /* tp_richcompare */
+        0,                        		               /* tp_weaklistoffset */
+        0,		                                       /* tp_iter */
+        0,                        		               /* tp_iternext */
+        PyDapEventsObjectSocket_methods,                   /* tp_methods */
+        0,                                                 /* tp_members */
+        0,                                                 /* tp_getset */
+        0,                                                 /* tp_base */
+        0,                                                 /* tp_dict */
+        0,                                                 /* tp_descr_get */
+        0,                                                 /* tp_descr_set */
+        0,                                                 /* tp_dictoffset */
+        0,                                                 /* tp_init */
+        0,                                                 /* tp_alloc */
+        PyType_GenericNew,                                 /* tp_new */
+};
+
 PyObject *dap_events_socket_create_after_py(PyDapEventsSocketObject *self){
     if (self->t_events_socket == NULL){
         return NULL;
