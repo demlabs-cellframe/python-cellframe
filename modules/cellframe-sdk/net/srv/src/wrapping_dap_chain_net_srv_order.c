e22e69972239510821934d6a2693342b5c419968
alexey.stratulat <alexey.stratulat@demlabs.net>
2022-03-15 15:44:51 +0700

    [*] Changed statics to globals in cellframe-sdk/net/srv module.

--- modules/cellframe-sdk/net/srv/src/wrapping_dap_chain_net_srv_order.c
+++ modules/cellframe-sdk/net/srv/src/wrapping_dap_chain_net_srv_order.c
@@ -2,6 +2,75 @@
 
 #define WRAPPING_DAP_CHAIN_NET_SRV_ORDER(a) ((PyDapChainNetSrvOrderObject*)a)
 
+PyMethodDef DapChainNetSrvOrderMethods[]={
+        {"size", (PyCFunction)wrapping_dap_chain_net_srv_order_get_size, METH_VARARGS, ""},
+        {"find", (PyCFunction)wrapping_dap_chain_net_srv_order_find, METH_VARARGS | METH_STATIC, ""},
+        {"delete", (PyCFunction)wrapping_dap_chain_net_srv_order_delete, METH_VARARGS | METH_STATIC, ""},
+        {"save", (PyCFunction)wrapping_dap_chain_net_srv_order_save, METH_VARARGS, ""},
+        {"getGdbGroup", (PyCFunction)wrapping_dap_chain_net_srv_order_get_gdb_group, METH_VARARGS | METH_STATIC, ""},
+        {"getNodelistGroup", (PyCFunction)wrapping_dap_chain_net_srv_order_get_nodelist_group, METH_VARARGS | METH_STATIC, ""},
+        {"addNotify", (PyCFunction)wrapping_dap_chain_net_srv_order_add_notify_callback, METH_VARARGS | METH_STATIC, ""},
+        {NULL, NULL, 0, NULL}
+};
+
+PyGetSetDef DapChaiNetSrvOrderGetsSets[] = {
+        {"version", (getter)wrapping_dap_chain_net_srv_order_get_version, NULL, NULL, NULL},
+        {"uid", (getter)wrapping_dap_chain_net_srv_order_get_srv_uid, NULL, NULL, NULL},
+        {"direction", (getter)wrapping_dap_chain_net_srv_order_get_srv_direction, NULL, NULL, NULL},
+        {"nodeAddr", (getter)wrapping_dap_chain_net_srv_order_get_srv_node_addr, NULL, NULL, NULL},
+        {"condHash", (getter)wrapping_dap_chain_net_srv_order_get_srv_tx_cond_hash, NULL, NULL, NULL},
+        {"priceUnit", (getter)wrapping_dap_chain_net_srv_order_get_srv_price_unit, NULL, NULL, NULL},
+        {"tsCreated", (getter)wrapping_dap_chain_net_srv_order_get_srv_ts_created, NULL, NULL, NULL},
+        {"tsExpires", (getter)wrapping_dap_chain_net_srv_order_get_srv_ts_expires, NULL, NULL, NULL},
+        {"srvPrice", (getter)wrapping_dap_chain_net_srv_order_get_srv_price, NULL, NULL, NULL},
+        {"srvPriceTicker", (getter)wrapping_dap_chain_net_srv_order_get_srv_price_ticker, NULL, NULL, NULL},
+        {"extSize", (getter)wrapping_dap_chain_net_srv_order_get_srv_ext_size, NULL, NULL, NULL},
+        {"extSign", (getter)wrapping_dap_chain_net_srv_order_get_srv_ext_n_sign, NULL, NULL, NULL},
+        {NULL}
+};
+
+PyTypeObject DapChainNetSrvOrderObject_DapChainNetSrvOrderObjectType = {
+        PyVarObject_HEAD_INIT(NULL, 0)
+        "CellFrame.ChainNetSrvOrder",        /* tp_name */
+        sizeof(PyDapChainNetSrvOrderObject), /* tp_basicsize */
+        0,                                /* tp_itemsize */
+        0,                                /* tp_dealloc */
+        0,                                /* tp_print */
+        0,                                /* tp_getattr */
+        0,                                /* tp_setattr */
+        0,                                /* tp_reserved */
+        0,                                /* tp_repr */
+        0,                                /* tp_as_number */
+        0,                                /* tp_as_sequence */
+        0,                                /* tp_as_mapping */
+        0,                                /* tp_hash  */
+        0,                                /* tp_call */
+        0,                                /* tp_str */
+        0,                                /* tp_getattro */
+        0,                                /* tp_setattro */
+        0,                                /* tp_as_buffer */
+        Py_TPFLAGS_DEFAULT |
+        Py_TPFLAGS_BASETYPE,          /* tp_flags */
+        "Chain net srv client object",               /* tp_doc */
+        0,		                          /* tp_traverse */
+        0,		                          /* tp_clear */
+        0,		                          /* tp_richcompare */
+        0,                                /* tp_weaklistoffset */
+        0,		                          /* tp_iter */
+        0,		                          /* tp_iternext */
+        DapChainNetSrvOrderMethods,        /* tp_methods */
+        0,                                /* tp_members */
+        DapChaiNetSrvOrderGetsSets,        /* tp_getset */
+        0,                                /* tp_base */
+        0,                                /* tp_dict */
+        0,                                /* tp_descr_get */
+        0,                                /* tp_descr_set */
+        0,                                /* tp_dictoffset */
+        (initproc)PyDapChainNetSrvOrder_init, /* tp_init */
+        0,                                /* tp_alloc */
+        PyType_GenericNew,                /* tp_new */
+};
+
 
 typedef struct _wrapping_list_func_callables{
     PyObject *func;
