0eba5c70938de6267ecee49616ac32f1fbe3241b
alexey.stratulat <alexey.stratulat@demlabs.net>
2022-03-15 20:40:21 +0700

    [*] Changed statics to globals in dap-sdk/net/server/enc module.

--- modules/dap-sdk/net/server/enc/src/wrapping_dap_enc_http.c
+++ modules/dap-sdk/net/server/enc/src/wrapping_dap_enc_http.c
@@ -1,5 +1,64 @@
 #include "wrapping_dap_enc_http.h"
 
+static PyMethodDef PyDapEncServerMethods[] = {
+        {"requestDecode", enc_http_request_decode_py, METH_VARARGS | METH_STATIC, ""},
+        {"isNull", enc_http_is_null_py, METH_NOARGS, ""},
+        {"reply", enc_http_reply_py, METH_VARARGS, ""},
+        {"replyEncode", enc_http_reply_encode_py, METH_VARARGS, ""},
+        {"addProc", enc_http_add_proc_py, METH_VARARGS | METH_STATIC, ""},
+        {NULL, NULL, 0, NULL}
+};
+
+static PyGetSetDef PyDapEncServerGetSet[] = {
+        {"action", (getter)enc_http_get_action_py, NULL, "Return action request", NULL},
+        {"request", (getter)enc_http_get_request_py, NULL, "Return request in view bytes", NULL},
+        {"urlPath", (getter)enc_http_get_url_path_py, NULL, "Return request in view bytes", NULL},
+        {"query", (getter)enc_http_get_in_query_py, NULL, "Return request in view bytes", NULL},
+        {NULL}
+};
+
+static PyTypeObject DapEncServer_DapEncServerType = {
+        PyVarObject_HEAD_INIT(NULL, 0)
+        "CellFrame.EncHttp",             /* tp_name */
+        sizeof(PyDapEncServerObject),                   /* tp_basicsize */
+        0,                                                 /* tp_itemsize */
+        (destructor)enc_http_delegate_delete_py,           /* tp_dealloc */
+        0,                                                 /* tp_print */
+        0,                                                 /* tp_getattr */
+        0,                                                 /* tp_setattr */
+        0,                                                 /* tp_reserved */
+        0,                                                 /* tp_repr */
+        0,                                                 /* tp_as_number */
+        0,                                                 /* tp_as_sequence */
+        0,                                                 /* tp_as_mapping */
+        0,                                                 /* tp_hash  */
+        0,                                                 /* tp_call */
+        0,                                                 /* tp_str */
+        0,                                                 /* tp_getattro */
+        0,                                                 /* tp_setattro */
+        0,                                                 /* tp_as_buffer */
+        Py_TPFLAGS_DEFAULT |
+        Py_TPFLAGS_BASETYPE,                           /* tp_flags */
+        "Dap enc server object",                         /* tp_doc */
+        0,		                                       /* tp_traverse */
+        0,                        		               /* tp_clear */
+        0,		                                       /* tp_richcompare */
+        0,                        		               /* tp_weaklistoffset */
+        0,		                                       /* tp_iter */
+        0,                        		               /* tp_iternext */
+        PyDapEncServerMethods,                   /* tp_methods */
+        0,                                                 /* tp_members */
+        PyDapEncServerGetSet,                              /* tp_getset */
+        0,                                                 /* tp_base */
+        0,                                                 /* tp_dict */
+        0,                                                 /* tp_descr_get */
+        0,                                                 /* tp_descr_set */
+        0,                                                 /* tp_dictoffset */
+        0,                                                 /* tp_init */
+        0,                                                 /* tp_alloc */
+        PyType_GenericNew,                                 /* tp_new */
+};
+
 PyObject *enc_http_reply_py(PyObject *self, PyObject *args){
     PyObject *l_obj_bytes;
     if (!PyArg_ParseTuple(args, "O", &l_obj_bytes)){
