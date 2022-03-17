78246958d0be93f158eeb85f6a76ae6281310ab8
alexey.stratulat <alexey.stratulat@demlabs.net>
2022-03-18 01:15:08 +0700

    [+] A wrapper was added over the function for setting the callback and changes in the round and a layer was made for the handler function, this callback, which generates data and calls the Python function.

---
+++ modules/cellframe-sdk/consensus/dag_poa/src/wrapping_dap_chain_cs_dag_poa.c
@@ -0,0 +1,99 @@
+#include "wrapping_dap_chain_cs_dag_poa.h"
+
+#define LOG_TAG "wrapping_dao_chain_cs_dag_poa"
+
+PyMethodDef DapChainCsDagPoaMethods[] = {
+        {"setPresign", (PyCFunction)wrapping_dap_chain_cs_dag_poa_presign_callback_set, METH_VARARGS | METH_STATIC, ""},
+        {NULL, NULL, 0, NULL}
+};
+
+PyTypeObject DapChainCsDagPoaObject_DapChainCsDagPoaObjectType = {
+        PyVarObject_HEAD_INIT(NULL, 0)
+        "CellFrame.ChainCsDagPoa",            /* tp_name */
+        sizeof(PyDapChainCsDagPoaObject),     /* tp_basicsize */
+        0,                               /* tp_itemsize */
+        0,                               /* tp_dealloc */
+        0,                               /* tp_print */
+        0,                               /* tp_getattr */
+        0,                               /* tp_setattr */
+        0,                               /* tp_reserved */
+        0,                               /* tp_repr */
+        0,                               /* tp_as_number */
+        0,                               /* tp_as_sequence */
+        0,                               /* tp_as_mapping */
+        0,                               /* tp_hash  */
+        0,                               /* tp_call */
+        0,               /* tp_str */
+        0,                               /* tp_getattro */
+        0,                               /* tp_setattro */
+        0,                               /* tp_as_buffer */
+        Py_TPFLAGS_DEFAULT |
+        Py_TPFLAGS_BASETYPE,         /* tp_flags */
+        "Chain net object",              /* tp_doc */
+        0,		                         /* tp_traverse */
+        0,		                         /* tp_clear */
+        0,		                         /* tp_richcompare */
+        0,                               /* tp_weaklistoffset */
+        0,		                         /* tp_iter */
+        0,		                         /* tp_iternext */
+        DapChainCsDagPoaMethods,              /* tp_methods */
+        0,                               /* tp_members */
+        0,                               /* tp_getset */
+        0,                               /* tp_base */
+        0,                               /* tp_dict */
+        0,                               /* tp_descr_get */
+        0,                               /* tp_descr_set */
+        0,                               /* tp_dictoffset */
+        0,                               /* tp_init */
+        0,                               /* tp_alloc */
+        PyType_GenericNew,               /* tp_new */
+};
+
+int _wrapping_callback_handler(dap_chain_t *a_chain, dap_chain_cs_dag_event_t *a_event, size_t a_event_size, void* a_arg){
+    if(!PyCallable_Check(a_arg)){
+        return -1;
+    }
+    PyGILState_STATE state = PyGILState_Ensure();
+    PyDapChainObject *l_obj_chain = PyObject_New(PyDapChainObject, &dapChainObject_dapChainType);
+    l_obj_chain->chain_t = a_chain;
+    PyDapChainCsDagEventObject *l_obj_event = PyObject_New(PyDapChainCsDagEventObject, &DapChainCsDagEvent_DapChainCsDagEventType);
+    l_obj_event->event = a_event;
+    l_obj_event->event_size = a_event_size;
+    PyObject *argv = Py_BuildValue("OO", l_obj_chain, l_obj_event);
+    Py_INCREF(argv);
+    Py_INCREF(a_arg);
+    PyObject *res = PyEval_CallObject(a_arg, argv);
+    Py_XDECREF(a_arg);
+    Py_XDECREF(argv);
+    PyGILState_Release(state);
+    if (res){
+        if (PyLong_AsLong(res)){
+            int l_res = _PyLong_AsInt(res);
+            return l_res;
+        } else {
+            return -3;
+        }
+    }else{
+        return -2;
+    }
+}
+
+PyObject* wrapping_dap_chain_cs_dag_poa_presign_callback_set(PyObject *self, PyObject *args){
+    (void)self;
+    PyObject *obj_chain;
+    PyObject *obj_func;
+    if (!PyArg_ParseTuple(args, "OO", &obj_chain, &obj_func)){
+        PyErr_SetString(PyExc_AttributeError, "Argument must be callable");
+        return NULL;
+    }
+    if (!PyDapChain_Check(obj_chain)){
+        PyErr_SetString(PyExc_AttributeError, "Argument must be callable");
+        return NULL;
+    }
+    if (!PyCallable_Check(obj_func)){
+        PyErr_SetString(PyExc_AttributeError, "Argument must be callable");
+        return NULL;
+    }
+    dap_chain_cs_dag_poa_presign_callback_set(((PyDapChainObject*)self)->chain_t, _wrapping_callback_handler, obj_func);
+    return Py_None;
+}
