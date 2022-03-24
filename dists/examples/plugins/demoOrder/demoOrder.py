6b943c76b45624417259542a69d27a1c0a5a8b6c
alexey.stratulat <alexey.stratulat@demlabs.net>
2022-03-24 23:25:38 +0700

    [*] Plugins have been migrated to use the new API. After refactoring, more precisely for importing and using new modules. And also this plugin was additionally documented with the help of comments.

--- dists/examples/plugins/demoOrder/demoOrder.py
+++ dists/examples/plugins/demoOrder/demoOrder.py
@@ -1,11 +1,12 @@
-from API_CellFrame import logItNotice, ChainNetSrvOrder, ChainNet, ChainNetSrvUID, ChainNetSrvOrderDirection, ChainNetSrvPriceUnitUID
-from API_CellFrame import Crypto, CryptoKeyType, AppCliServer, ChainHashFast
+from DAP.Core import logIt
+from DAP.Crypto import HashFast, Algo, CryptoKeyType
+from CellFrame.Network import Net, ServiceOrder, ServiceOrderDirection, ServiceUID, ServicePriceUnitUID
+from CellFrame import AppCliServer
 import time
 from datetime import datetime, timedelta
 
+"""This function is designed to get a string representation of the values that are in the order. """
 def infoOrder(order):
-#    hash = ChainHashFast.strToHashFast(hash_str)
-#    order = ChainNetSrvOrder.find(net, hash_str)
     reply = "Order: \n"
     reply += "\tVersion:" + str(order.version)+"\n"
     reply += "\tUID: " + str(order.uid) + "\n"
@@ -48,9 +49,9 @@ def infoOrder(order):
 def pwoCMD(argv, indexStrReply):
     print(indexStrReply)
     if (len(argv) == 5):
-        net = ChainNet.byName(argv[2])
+        net = Net.byName(argv[2])
         if argv[3] == "find":
-            order = ChainNetSrvOrder.find(net, argv[4])
+            order = ServiceOrder.find(net, argv[4])
             reply = ""
             if order is None:
                 reply = "Order with hash "+argv[4]+" not fined."
@@ -58,23 +59,9 @@ def pwoCMD(argv, indexStrReply):
                 reply = "Order: \n"
                 order_reply = infoOrder(order)
                 reply += order_reply
- #               reply += "\tVersion:" + str(order.version)+"\n"
-#                reply += "\tUID: " + str(order.uid) + "\n"
-#                reply += "\tDirection: " + str(order.direction) + "\n"
-#                reply += "\tNode addr: " + str(order.nodeAddr) + "\n"
-##                reply += "\tCond hash: " + str(order.condHash) + "\n"
-#                reply += "\tPrice unit: " + str(order.priceUnit) + "\n"
-#                created = datetime.fromtimestamp(order.tsCreated)
-#                reply += "\tCreated: " + str(created) + "\n"
-#                expires = datetime.fromtimestamp(order.tsExpires)
-#                reply += "\tExpires: " + str(expires) + "\n"
-#                reply += "\tSrv price: " + str(order.srvPrice) + "\n"
-#                reply += "\tSrv price ticker: " + order.srvPriceTicker + "\n"
-#                reply += "\text size:" + str(order.extSize) + "\n"
-#                reply += "\text:" + str(order.ext) + "\n"
             AppCliServer.setReplyText(reply, indexStrReply)
         elif argv[3] == "del":
-            res = ChainNetSrvOrder.delete(net, argv[4])
+            res = ServiceOrder.delete(net, argv[4])
             AppCliServer.setReplyText("Result: "+str(res)+".", indexStrReply)
         else:
             AppCliServer.setReplyText("An unknown action is specified to be performerd on the order.", indexStrReply)
@@ -82,40 +69,40 @@ def pwoCMD(argv, indexStrReply):
         AppCliServer.setReplyText("This command takes only four arguments.", indexStrReply)
 
 
-
+"""
+The infoCMD function is a handler for a command that displays information about an order by its hash. 
+"""
 def infoCMD(argv, indexStrReply):
     if (len(argv) == 2):
-        hash = ChainHashFast.strToHashFast(argv[1])
-        order = ChainNetSrvOrder.find(hash)
+        # We get an object of type HashFast from the 16-ary string representation of the hash.
+        hash = HashFast.strToHashFast(argv[1])
+        order = ServiceOrder.find(hash)
         reply = "Order: \n"
         order_reply = infoOrder(order)
         reply += order_reply
-#        reply += "\tVersion:" + str(order.version)+"\n"
-#        reply += "\tUID: " + str(order.uid) + "\n"
-#        reply += "\tDirection: " + str(order.direction) + "\n"
-#        reply += "\tNode addr: " + str(order.nodeAddr) + "\n"
-#        reply += "\tCond hash: " + str(order.condHash) + "\n"
-#        reply += "\tPrice unit: " + str(order.priceUnit) + "\n"
-#        created = datetime.fromtimestamp(order.tsCreated)
-#        reply += "\tCreated: " + str(created) + "\n"
-#        expires = datetime.fromtimestamp(order.tsExpires)
-#        reply += "\tExpires: " + str(expires) + "\n"
-#        reply += "\tSrv price: " + str(order.srvPrice) + "\n"
-#        reply += "\tSrv price ticker: " + order.srvPriceTicker + "\n"
-#        reply += "\text size:" + str(order.extSize) + "\n"
-#        reply += "\text:" + str(order.ext) + "\n"
         AppCliServer.setReplyText(reply, indexStrReply)
     else:
         AppCliServer.setReplyText("Order hash not set", indexStrReply)
 
 """
+This function generates the key required to create an order
+"""
+def create_key():
+    """The key generation function accepts the type of the generated key, kex_buf,
+    the size of the generated key, and the seed phrase. """
+    return Algo.generateNewKey(CryptoKeyType.DAP_ENC_KEY_TYPE_SIG_BLISS(), "SARON00151454_VDGTKHJFY", 512, " I SIMPKLE SEED STRING ")
 
 """
-
-def create_key():
-    return Crypto.generateNewKey(CryptoKeyType.DAP_ENC_KEY_TYPE_SIG_BLISS(), "SARON00151454_VDGTKHJFY", 512, " I SIMPKLE SEED STRING ")
-
-def order_notify(op_code, group, key, order):
+This function is a notifier, it is triggered when an order is created or deleted from GlobalDB.
+It takes the following arguments as input
+    * op_code is responsible for the state of the pian order or deleted.
+    * group name in globalDB.
+    * the key by which the value is located (usually a hash).
+    * The data itself, in this case it is the order object
+    * the object that was set when setting the notifier, it can be a string, a number, a function, etc. In this case, it is a string. 
+"""
+def order_notify(op_code, group, key, order, arg):
+    print("Arg: "+arg)
     print("Notify: \n")
     print("op_code: "+op_code+"\n")
     print("group: "+group+"\n")
@@ -126,36 +113,55 @@ def order_notify(op_code, group, key, order):
         print("No order.")
 
 def init():
-    logItNotice("Running plugin order")
-    net = ChainNet.byName("kelvin-testnet")
-#Getting info
-    ChainNetSrvOrder.addNotify(net, order_notify)
-    gdb_group = ChainNetSrvOrder.getGdbGroup(net)
-    logItNotice("Ordr in group: "+gdb_group)
-    node_list_group = ChainNetSrvOrder.getNodelistGroup(net)
-    logItNotice("Node list group: "+node_list_group)
-#Create order
-    print("net: "+ str(net))
-    net_srv = ChainNetSrvUID(12345)
-    direction = ChainNetSrvOrderDirection.getDirSell()
+    logIt.notice("Running plugin order")
+    net = Net.byName("kelvin-testnet")
+# We add a notifier that will work when creating or deleting an order.
+    ServiceOrder.addNotify(net, order_notify, "This is the same line that will be printed out when the notifier "
+                                              "argument is printed. ")
+# We receive and display information about the group where orders are stored, as well as about the nodelist.
+    gdb_group = ServiceOrder.getGdbGroup(net)
+    logIt.notice("Ordr in group: "+gdb_group)
+    node_list_group = ServiceOrder.getNodelistGroup(net)
+    logIt.notice("Node list group: "+node_list_group)
+# Creating a new order
+    print("net: " + str(net))
+#   Set the direction of the order and the UID of the service.
+    net_srv = ServiceUID(12345)
+    direction = ServiceOrderDirection.getDirSell()
     print("direction" + str(direction))
+#   We get the address of the node.
     node_addr = net.getCurAddr()
-    tx_cond = "TXCOND"
+    """tx_cond  - This should be the address of the conditional transaction, but it is not required to create an order.
+    But in the order that the service issues, it should be."""
+#   We set the price and units of measurement, for which you buy for this price.
     price = 531
-    priceUnitUid = ChainNetSrvPriceUnitUID.mb()
-    print ("Price Unit UID:" + str(priceUnitUid))
+    priceUnitUid = ServicePriceUnitUID.mb()
+    print("Price Unit UID:" + str(priceUnitUid))
     key = create_key()
-    #Expires
+#   We determine the lifetime of the order, at the moment it is not used in the SDK.
     dt_exp = datetime.now()
-    dt_delta = timedelta(days = 30)
+    dt_delta = timedelta(days=30)
     dt_exp += dt_delta
-    print (dt_exp)
-    print (dt_exp.timestamp())
+    print(dt_exp)
+    print(dt_exp.timestamp())
     ts_expires = dt_exp.timestamp()
     print(type(ts_expires))
-    order = ChainNetSrvOrder(net, direction, net_srv, node_addr, None, 100, priceUnitUid, "tCELL", ts_expires, "http://myvi.com/?QBS".encode('utf-8'), key)
+    """
+    With the help of this constructor, an order is created. The constructor takes the following parameters as input:
+        * Network object.
+        * Direction order
+        * Service UID.
+        * Node address.
+        * Hash of the conditional transaction in this case None.
+        * Price.
+        * Units of measure.
+        * A set of bytes, there can be any information in this case, the URL address.
+        * The key with which the order is signed. 
+    """
+    order = ServiceOrder(net, direction, net_srv, node_addr, None, price, priceUnitUid, "tCELL", ts_expires, "http://myvi.com/?QBS".encode('utf-8'), key)
+#   This function simply saves the order and returns its hash, by which the order can be found or deleted.
     hash = order.save(net)
-    logItNotice("HASH order:"+str(hash))
+    logIt.notice("HASH order:"+str(hash))
     AppCliServer.cmdItemCreate("pwo", pwoCMD, "Example command for working with orders through the plugin",
 """
     Arguments for command:
