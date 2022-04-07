9ae7766a26b65aece4efcfe59481fa26c7876157
alexey.stratulat <alexey.stratulat@demlabs.net>
2022-04-07 19:02:58 +0700

    [+] Added tests for correct operation of functions for getting ChainAddr and HashFast objects from a string. The check goes to the fact that if the input data is a string, but of an incorrect format, then the function returns None, otherwise an object of the desired type is returned, for which the inverse conversion to a string is performed. And then the result is compared with the original value.

--- dists/unit-tests/unit_tests.py
+++ dists/unit-tests/unit_tests.py
@@ -1,4 +1,6 @@
 from DAP.Core import logIt
+from DAP.Crypto import HashFast
+from CellFrame.Chain import ChainAddr
 import sys
 import modules.demoCustomCMD as customCMD
 
@@ -35,6 +37,15 @@ def init():
     logIt.notice("Registering features to be tested.")
     unittest.registration("Test pass", test_pass, 1)
     unittest.registration("Test for creating a custom CLI command", customCMD.init, 0)
+    unittest.registration(
+        "Test for getting a HashFast object from a string",
+        test_get_hash_fast,
+        "0x795327F8B194E24BC6067F5C95DB5B31A74C7CAF28AD74D0A805C960192BD57E")
+    unittest.registration(
+        "Test for getting an instance of an object of type ChainAddr from a string",
+        test_get_chain_addr,
+        "mWNv7A43YnqRHCWVJCb2oR1ZaBwaiDQAgZzf3UjXGercDkFwre8z7ShMpsWRRRjRJfdJfQbs8EUqDzKG232a4fpahKBDjfV8ru1LeWTP"
+    )
     unittest.registration("Test FAIL", test_pass, 0)
     len_tests = len(unittest.test)
     logIt.notice("Start testing "+str(len_tests)+" modules (functions).")
@@ -52,3 +63,20 @@ def init():
 
 def test_pass():
     return 1
+
+def test_get_hash_fast():
+    ch_hf = HashFast.fromString("abc")
+    if ch_hf is not None:
+        return False
+    ch_hf = HashFast.fromString("0x03FKEN02E0932JF")
+    if ch_hf is not None:
+        return False
+    ch_hf = HashFast.fromString("0x795327F8B194E24BC6067F5C95DB5B31A74C7CAF28AD74D0A805C960192BD57E")
+    return str(ch_hf)
+
+def test_get_chain_addr():
+    addr = ChainAddr.fromStr("mFJJEF2312AAGIWJIEc1885841")
+    if addr is not None:
+        return False
+    addr = ChainAddr.fromStr("mWNv7A43YnqRHCWVJCb2oR1ZaBwaiDQAgZzf3UjXGercDkFwre8z7ShMpsWRRRjRJfdJfQbs8EUqDzKG232a4fpahKBDjfV8ru1LeWTP")
+    return str(addr)
