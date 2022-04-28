54aa5fc99305521d662ec09427792f1b8286edd3
alexey.stratulat <alexey.stratulat@demlabs.net>
2022-04-28 19:24:58 +0700

    [*] Redesigned Unit tests, now it creates commands for executing the run and list commands.

--- dists/unit-tests/unit_tests.py
+++ dists/unit-tests/unit_tests.py
@@ -1,7 +1,8 @@
 from DAP.Core import logIt
 from DAP.Crypto import HashFast
 from CellFrame.Chain import ChainAddr
-import sys
+from CellFrame import AppCliServer
+import sys, time
 import modules.demoCustomCMD as customCMD
 import modules.mathTest.mathTest as mathTest
 
@@ -32,9 +33,41 @@ class UnitTests:
         test = UnitTest(name, func, result)
         self.test.append(test)
 
+unittest = UnitTests()
+
+def runTestCMD(argv, indexStrReply):
+    if len(argv) == 2:
+        if argv[1] == "list":
+            reply = "The following unit tests are registered:\n"
+            i = 1
+            for test in unittest.test:
+                reply += "\t"+str(i)+". "+test.name+"\n"
+                i += 1
+            AppCliServer.setReplyText(reply, indexStrReply)
+        elif argv[1] == "run":
+            len_tests = len(unittest.test)
+            logIt.notice("Start testing "+str(len_tests)+" modules (functions).")
+            i = 0
+            for test in unittest.test:
+                i += 1
+                logIt.info("["+str(i)+"/"+str(len_tests)+"] "+test.name)
+                res = test.run()
+                if res[0] == False:
+                    logIt.error("Test FAILED! \n Err: "+res[1])
+                    reply = "The "+ test.name +" test ended with an error."
+                    time.sleep(2)
+                    AppCliServer.setReplyText(reply, indexStrReply)
+                    sys.exit(-1)
+                else:
+                    logIt.notice("Test PASS!")
+            reply =  "All tests were successfully completed."
+            AppCliServer.setReplyText(reply, indexStrReply)
+    else:
+        reply = "This function takes a run and a list argument."
+        AppCliServer.setReplyText(reply, indexStrReply)
+
 def init():
     logIt.notice("Launching the unit testing plugin.")
-    unittest = UnitTests()
     logIt.notice("Registering features to be tested.")
     unittest.registration("Test pass", test_pass, 1)
     unittest.registration("Test for creating a custom CLI command", customCMD.init, 0)
@@ -49,18 +82,13 @@ def init():
     )
     unittest.registration("256-bit math test", mathTest.run_unit_test, True)
     unittest.registration("Test FAIL", test_pass, 0)
-    len_tests = len(unittest.test)
-    logIt.notice("Start testing "+str(len_tests)+" modules (functions).")
-    i = 0
-    for test in unittest.test:
-        i += 1
-        logIt.info("["+str(i)+"/"+str(len_tests)+"] "+test.name)
-        res = test.run()
-        if res[0] == False:
-            logIt.error("Test FAILED! \n Err: "+res[1])
-            sys.exit(-1)
-        else:
-            logIt.notice("Test PASS!")
+    AppCliServer.cmdItemCreate("unit_tests", runTestCMD, "Command for working with Unit tests.", 
+    """
+        This function takes a run parameter and a list
+        Parameter:
+            * run - runs unit tests sequentially.
+            * list - returns a list of registered unit tests.
+    """)
     return 0
 
 def test_pass():
