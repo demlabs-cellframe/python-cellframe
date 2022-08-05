9e1dd07cefe50e86b6afa02782e7a76db77aa8d9
Dmitry Puzyrkov <dmitry.puzyrkov@demlabs.net>
2022-08-05 13:39:20 +0000

    PythonPath to /opt/cellframe and its submodules

--- modules/plugins/src/dap_chain_plugins.c
+++ modules/plugins/src/dap_chain_plugins.c
@@ -45,6 +45,15 @@ int dap_chain_plugins_init(dap_config_t *a_config){
         PyEval_InitThreads();
         PyObject *l_sys_module = PyImport_ImportModule("sys");
         s_sys_path = PyObject_GetAttrString(l_sys_module, "path");
+
+        #ifdef DAP_BUILD_WITH_PYTHON_ENV
+            PyObject *l_obj_dir_path_dynload = PyUnicode_FromString("/opt/cellframe-node/lib/python3.7/lib-dynload");
+            PyList_Append(s_sys_path, l_obj_dir_path_dynload);
+
+            PyObject *l_obj_dir_path_sp = PyUnicode_FromString("/opt/cellframe-node/lib/python3.7/site-packages");
+            PyList_Append(s_sys_path, l_obj_dir_path_sp);
+        #endif    
+
         //Get list files
         dap_list_name_directories_t *l_list_plugins_name = dap_get_subs(s_plugins_root_path);
         dap_list_name_directories_t *l_element;
