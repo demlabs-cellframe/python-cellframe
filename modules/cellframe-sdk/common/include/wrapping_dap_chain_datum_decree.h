e91fa5d72fef2b3981940922650687ff8c6b918c
alexey.stratulat <alexey.stratulat@demlabs.net>
2023-03-24 15:10:06 +0700

    [+] Added constructor for decree datum, added method for adding signature for decree datum. Added properties to get signatures, creation date, type and subtype.

--- modules/cellframe-sdk/common/include/wrapping_dap_chain_datum_decree.h
+++ modules/cellframe-sdk/common/include/wrapping_dap_chain_datum_decree.h
@@ -10,8 +10,12 @@ typedef struct PyDapChainDatumDecree{
 
 PyObject* wrapping_dap_chain_datum_decree_get_ts_created(PyObject *self, void* closure);
 PyObject* wrapping_dap_chain_datum_decree_get_type(PyObject *self, void* closure);
-PyObject* wrapping_dap_chain_datum_decree_get_sub_type(PyObject *self, void* closure);
+PyObject* wrapping_dap_chain_datum_decree_get_type_str(PyObject *self, void* closure);
+PyObject* wrapping_dap_chain_datum_decree_get_subtype(PyObject *self, void* closure);
+PyObject* wrapping_dap_chain_datum_decree_get_subtype_str(PyObject *self, void* closure);
 PyObject* wrapping_dap_chain_datum_decree_get_data(PyObject *self, void* closure);
 PyObject* wrapping_dap_chain_datum_decree_get_signs(PyObject *self, void* closure);
+
+PyObject *wrapping_dap_chain_datum_decree_add_sign(PyObject *self, PyObject *args);
 
 extern PyTypeObject DapChainDatumDecreeObjectType;
