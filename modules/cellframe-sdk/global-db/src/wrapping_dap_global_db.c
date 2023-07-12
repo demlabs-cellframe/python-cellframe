2fdd6ee69f052a4daa14ca26bc90b7b40d69e0f6
Olzhas <olzhas@iMac-demlabs.local>
2023-07-12 20:19:06 +0700

    [+] memcheck

--- modules/cellframe-sdk/global-db/src/wrapping_dap_global_db.c
+++ modules/cellframe-sdk/global-db/src/wrapping_dap_global_db.c
@@ -175,6 +175,8 @@ void pvt_wrapping_dap_global_db_add_sync_extra_group_func_callback(dap_global_db
     if (!a_arg)
         return;
     _wrapping_dap_global_db_add_sync_extra_group_callback_t *l_obj = DAP_NEW(_wrapping_dap_global_db_add_sync_extra_group_callback_t);
+    if (!l_obj) 
+        return;
     l_obj->store_obj = dap_store_obj_copy(a_obj, 1);
     l_obj->func = ((_wrapping_dap_global_db_add_sync_extra_group_callback_t*)a_arg)->func;
     l_obj->arg = ((_wrapping_dap_global_db_add_sync_extra_group_callback_t*)a_arg)->arg;
