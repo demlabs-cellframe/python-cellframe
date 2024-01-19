299194a9e8edb6c222f9fafade6ef7a443d628c5
pavel.uhanov <pavel.uhanov@demlabs.net>
2024-01-19 12:10:39 +0300

    [*] adopt to dap_server_new func

--- modules/dap-sdk/io/src/dap_server_python.c
+++ modules/dap-sdk/io/src/dap_server_python.c
@@ -36,15 +36,33 @@ void py_server_deinit(void){
 
 static PyObject *py_server_listen(PyObject *self, PyObject *args)
 {
-    const char *addr;
-    uint16_t port;
-    uint16_t type;
-    if (!PyArg_ParseTuple(args, "sHH", &addr, &port, &type)){
+    PyObject *obj_addrs;
+    uint16_t l_count = 0;
+    uint16_t l_type = 0;
+    if (!PyArg_ParseTuple(args, "OHH", &obj_addrs, &l_count, &l_type) || !l_count || l_type > 1){
         return NULL;
     }
-    if (type > 1)
-        return  NULL;
+
+    if (!PyList_Check(obj_addrs)) {
+        PyErr_SetString(PyExc_AttributeError, "An invalid function argument was specified. The first argument must be "
+                                              "an array of server addresses.");
+        return NULL;
+    }
+
+    char **l_addrs = DAP_NEW_Z_COUNT(char *, l_count);
+    for (uint16_t i = 0; i < l_count; i++){
+        char *l_current_addr;
+        if (!PyArg_ParseTuple(PyList_GetItem(obj_addrs, i), "s", &l_current_addr)) {
+            char *l_str_err = dap_strdup_printf("The %zu element in the list of server addresses is not a string.", i);
+            PyErr_SetString(PyExc_AttributeError, l_str_err);
+            DAP_DELETE(l_str_err);
+            DAP_DELETE(l_addrs);
+            return NULL;
+        }
+        l_addrs[i] = l_current_addr;
+    }
     PyObject *obj = _PyObject_New(&DapServerObjectType);
-    ((PyDapServerObject*)obj)->t_server = dap_server_new( addr, port, type, NULL);
+    ((PyDapServerObject*)obj)->t_server = dap_server_new(l_addrs, l_count, l_type, NULL);
+    DAP_DELETE(l_addrs);
     return Py_BuildValue("O", obj);
 }
