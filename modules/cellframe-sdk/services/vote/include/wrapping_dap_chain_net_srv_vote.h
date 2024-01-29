fbc91b07ff376cf267f4204fcf5b7b411a71b063
alexey.stratulat <alexey.stratulat@demlabs.net>
2024-01-29 16:56:45 +0700

    [+] Added bases for creating wrapping service voting.

---
+++ modules/cellframe-sdk/services/vote/include/wrapping_dap_chain_net_srv_vote.h
@@ -0,0 +1,11 @@
+#include "Python.h"
+
+typedef struct PyDapChainNetSrvVote{
+	PyObject_HEAD
+}PyDapChainNetSrvVoteObject;
+
+PyObject *wrapping_dap_chain_net_srv_vote_create(PyObject *self, PyObject *args);
+PyObject *wrapping_dap_chain_net_srv_vote_list(PyObject *self, PyObject *args);
+PyObject *wrapping_dap_chain_net_srv_vote(PyObject *self, PyObject *args);
+
+extern PyTypeObject PyDapChainNetSrvVoteObjectType;
---
+++ modules/cellframe-sdk/services/vote/src/wrapping_dap_chain_net_srv_vote.c
@@ -0,0 +1,5 @@
+#include "wrapping_dap_chain_net_srv_vote.h"
+
+PyObject *wrapping_dap_chain_net_srv_vote_list(PyObject *self, PyObject *args){
+    PyObject *obj_net;
+}
