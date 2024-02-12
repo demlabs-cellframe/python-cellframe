327a2d7f9e333b4b07c9e96b81b787bae5a236cf
alexey.stratulat <alexey.stratulat@demlabs.net>
2024-02-12 15:27:21 +0700

    [*] Fixed building.

--- modules/cellframe-sdk/common/include/wrapping_dap_chain_datum_tx_voting.h
+++ modules/cellframe-sdk/common/include/wrapping_dap_chain_datum_tx_voting.h
@@ -22,6 +22,7 @@
     You should have received a copy of the GNU General Public License
     along with any DAP based project.  If not, see <http://www.gnu.org/licenses/>.
 */
+#pragma once
 #include "Python.h"
 #include "dap_chain_datum_tx_voting.h"
 
