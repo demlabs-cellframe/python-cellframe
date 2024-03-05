f95146b602f112947aa3ded4c3a84033d7833759
alexey.stratulat <alexey.stratulat@demlabs.net>
2024-03-05 11:12:45 +0700

    [*] In the wrapping_dap_chain_net_srv_xchange_get_orders function, I reworked the receipt of the price list. Now we immediately create a sheet of the required size and fill it out.

--- modules/cellframe-sdk/services/xchange/wrapping_dap_chain_net_srv_xchange.c
+++ modules/cellframe-sdk/services/xchange/wrapping_dap_chain_net_srv_xchange.c
@@ -28,14 +28,15 @@ PyObject *wrapping_dap_chain_net_srv_xchange_get_orders(PyObject *self, PyObject
         return NULL;
     }
     dap_list_t *l_list_prices = dap_chain_net_srv_xchange_get_prices(((PyDapChainNetObject*)obj_net)->chain_net);
+    size_t l_list_prices_count = dap_list_length(l_list_prices);
     dap_list_t *tmp = l_list_prices;
-    PyObject *obj_list_price = PyList_New(0);
-    while (tmp) {
+    PyObject *obj_list_price = PyList_New((Py_ssize_t)l_list_prices_count);
+    for (size_t i = 0; i < l_list_prices_count; i++){
         dap_chain_net_srv_xchange_price_t *l_price = (dap_chain_net_srv_xchange_price_t*)tmp->data;
         PyDapChainNetSrvXchangeOrderObject *nob = PyObject_New(PyDapChainNetSrvXchangeOrderObject, &PyDapChainNetSrvXchangeOrderObjectType);
         nob->price = DAP_NEW(dap_chain_net_srv_xchange_price_t);
         memcpy(nob->price, l_price, sizeof (dap_chain_net_srv_xchange_price_t ));
-        PyList_Append(obj_list_price, nob);
+        PyList_SetItem(obj_list_price, (Py_ssize_t)i, (PyObject*)nob);
         tmp = tmp->next;
     }
     dap_list_free(l_list_prices);
