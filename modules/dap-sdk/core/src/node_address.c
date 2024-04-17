189aa7a105f2b7c3805a00b23ad1a1d1f0f2bcf4
alexey.stratulat <alexey.stratulat@demlabs.net>
2024-04-17 16:09:37 +0700

    [+] Added NodeAddr constructor, which takes a string and converts it to dap_stream_node_addr_t.

--- modules/dap-sdk/core/src/node_address.c
+++ modules/dap-sdk/core/src/node_address.c
@@ -1,5 +1,6 @@
 #include "node_address.h"
 #include "libdap-python.h"
+#include "dap_strfuncs.h"
 
 PyObject* PyDapNodeAddrObject_str(PyObject* self) {
     char *ret = dap_strdup_printf(NODE_ADDR_FP_STR, NODE_ADDR_FP_ARGS_S(((PyDapNodeAddrObject*) self)->addr));
@@ -8,6 +9,24 @@ PyObject* PyDapNodeAddrObject_str(PyObject* self) {
     return l_obj;
 }
 
+int PyDapNodeAddr_Init(PyDapNodeAddrObject *self, PyObject *args, PyObject *kwds) {
+    const char *in_addr_str;
+    char *kwords[] = {
+        "address",
+        NULL
+    };
+    if (!PyArg_ParseTupleAndKeywords(args, kwds, "s", kwords, &in_addr_str))
+        return -1;
+    if (!dap_stream_node_addr_from_str(&self->addr, in_addr_str)) {
+        return 0;
+    }
+    char *l_err = dap_strdup_printf("Failed to convert %s to NodeAddr, please check the recording format.", in_addr_str);
+    PyErr_SetString(PyExc_Exception, l_err);
+    DAP_DELETE(l_err);
+    return -1;
+}
+
 PyTypeObject DapNodeAddrObjectType = DAP_PY_TYPE_OBJECT("DAP.Core.NodeAddr", sizeof(PyDapNodeAddrObject),
                                                           "Stream node addr object", 
-                                                          .tp_str = PyDapNodeAddrObject_str);
+                                                          .tp_str = PyDapNodeAddrObject_str,
+                                                          .tp_init = (initproc)PyDapNodeAddr_Init);
