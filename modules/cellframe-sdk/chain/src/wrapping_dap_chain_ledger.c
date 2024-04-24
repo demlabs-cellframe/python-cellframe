9143d8ee68744214dd46ccd75866428c32367b52
alexey.stratulat <alexey.stratulat@demlabs.net>
2024-04-24 18:31:50 +0700

    [+] Added installation of a pointer to the ledger for the transaction datum.

--- modules/cellframe-sdk/chain/src/wrapping_dap_chain_ledger.c
+++ modules/cellframe-sdk/chain/src/wrapping_dap_chain_ledger.c
@@ -342,6 +342,7 @@ PyObject *dap_chain_ledger_tx_find_by_hash_py(PyObject *self, PyObject *args){
         return NULL;
     PyDapChainDatumTxObject *res = PyObject_NEW(PyDapChainDatumTxObject, &DapChainDatumTxObjectType);
     res->datum_tx = dap_ledger_tx_find_by_hash(((PyDapChainLedgerObject*)self)->ledger, ((PyDapHashFastObject*)h_fast)->hash_fast);
+    res->ledger = ((PyDapChainLedgerObject*)self)->ledger;
     res->original = false;
     if (res->datum_tx == NULL) {
         PyObject_DEL(res);
@@ -355,10 +356,11 @@ PyObject *dap_chain_ledger_tx_find_by_addr_py(PyObject *self, PyObject *args){
     PyObject *first_hash;
     if (!PyArg_ParseTuple(args, "s|O|O", &token, &addr, &first_hash))
         return NULL;
-    PyObject *res = _PyObject_New(&DapChainDatumTxObjectType);
-    ((PyDapChainDatumTxObject*)res)->datum_tx = dap_ledger_tx_find_by_addr(((PyDapChainLedgerObject*)self)->ledger, token, ((PyDapChainAddrObject*)addr)->addr, ((PyDapHashFastObject*)first_hash)->hash_fast);
-    ((PyDapChainDatumTxObject*)res)->original = false;
-    return Py_BuildValue("O", res);
+    PyDapChainDatumTxObject *res = PyObject_New(PyDapChainDatumTxObject, &DapChainDatumTxObjectType);
+    res->datum_tx = dap_ledger_tx_find_by_addr(((PyDapChainLedgerObject*)self)->ledger, token, ((PyDapChainAddrObject*)addr)->addr, ((PyDapHashFastObject*)first_hash)->hash_fast);
+    res->original = false;
+    res->ledger = ((PyDapChainLedgerObject*)self)->ledger;
+    return (PyObject*)res;
 }
 PyObject *dap_chain_ledger_tx_find_by_pkey_py(PyObject *self, PyObject *args){
     char *p_key;
@@ -366,28 +368,30 @@ PyObject *dap_chain_ledger_tx_find_by_pkey_py(PyObject *self, PyObject *args){
     PyObject *obj_first_hash;
     if (!PyArg_ParseTuple(args, "s|n|O", &p_key, &p_key_size, &obj_first_hash))
         return NULL;
-    PyObject *res = _PyObject_New(&DapChainDatumTxObjectType);
-    ((PyDapChainDatumTxObject*)res)->datum_tx = (dap_chain_datum_tx_t*)dap_ledger_tx_find_by_pkey(
+    PyDapChainDatumTxObject *res = PyObject_New(PyDapChainDatumTxObject, &DapChainDatumTxObjectType);
+    res->datum_tx = (dap_chain_datum_tx_t*)dap_ledger_tx_find_by_pkey(
                 ((PyDapChainLedgerObject*)self)->ledger,
                 p_key,
                 p_key_size,
                 ((PyDapHashFastObject*)obj_first_hash)->hash_fast);
-    ((PyDapChainDatumTxObject*)res)->original = false;
+    res->original = false;
+    res->ledger = ((PyDapChainLedgerObject*)self)->ledger;
     return Py_BuildValue("O", res);
 }
 PyObject *dap_chain_ledger_tx_cache_find_out_cond_py(PyObject *self, PyObject *args){
     PyObject *obj_first_hash;
     if (!PyArg_ParseTuple(args, "O", &obj_first_hash))
         return NULL;
-    PyObject *res = _PyObject_New(&DapChainDatumTxObjectType);
     dap_chain_tx_out_cond_t **out_conds = NULL;
     int *out_cond_idx = NULL;
-    ((PyDapChainDatumTxObject*)res)->datum_tx = (dap_chain_datum_tx_t*)dap_ledger_tx_cache_find_out_cond(
+    PyDapChainDatumTxObject *res = PyObject_New(PyDapChainDatumTxObject, &DapChainDatumTxObjectType);
+    res->datum_tx = (dap_chain_datum_tx_t*)dap_ledger_tx_cache_find_out_cond(
                 ((PyDapChainLedgerObject*)self)->ledger,
                 DAP_CHAIN_TX_OUT_COND_SUBTYPE_SRV_PAY,  //TODO: support other subtypes
                 ((PyDapHashFastObject*)obj_first_hash)->hash_fast,
                 out_conds, out_cond_idx, NULL);
-    ((PyDapChainDatumTxObject*)res)->datum_tx = false;
+    res->original = false;
+    res->ledger = ((PyDapChainLedgerObject*)self)->ledger;
     return Py_BuildValue("O", res);
 }
 
@@ -473,6 +477,7 @@ PyObject *dap_chain_ledger_get_txs_py(PyObject *self, PyObject *args){
         PyDapChainDatumTxObject *obj_tx = PyObject_New(PyDapChainDatumTxObject, &DapChainDatumTxObjectType);
         obj_tx->datum_tx = l_iter->data;
         obj_tx->original = false;
+        obj_tx->ledger = ((PyDapChainLedgerObject*)self)->ledger;
         PyList_SetItem(obj_list, i, (PyObject*)obj_tx);
     }
     dap_list_free(l_txs);
@@ -494,6 +499,8 @@ static void pvt_wrapping_dap_chain_ledger_tx_add_notify(void *a_arg, dap_ledger_
     PyDapChainDatumTxObject *obj_tx = PyObject_NEW(PyDapChainDatumTxObject, &DapChainDatumTxObjectType);
     obj_ledger->ledger = a_ledger;
     obj_tx->datum_tx = a_tx;
+    obj_tx->original = false;
+    obj_tx->ledger = a_ledger;
     PyObject *notify_arg = !notifier->argv ? Py_None : notifier->argv;
     PyObject *argv = Py_BuildValue("OOO", (PyObject*)obj_ledger, (PyObject*)obj_tx, notify_arg);
     log_it(L_DEBUG, "Call tx added ledger notifier for net %s", a_ledger->net->pub.name);
@@ -544,6 +551,8 @@ static bool s_python_obj_notifier(void *a_arg)
     obj_ledger->ledger = l_args->ledger;
     PyDapChainDatumTxObject *obj_tx = PyObject_NEW(PyDapChainDatumTxObject, &DapChainDatumTxObjectType);
     obj_tx->datum_tx = l_args->tx;
+    obj_tx->original = false;
+    obj_tx->ledger = obj_ledger->ledger;
     PyObject *l_notify_arg = !l_notifier->argv ? Py_None : l_notifier->argv;
     Py_INCREF(l_notify_arg);
     log_it(L_DEBUG, "Call bridged tx ledger notifier for net %s", l_args->ledger->net->pub.name);
@@ -608,7 +617,7 @@ PyObject *dap_chain_ledger_tx_hash_is_used_out_item(PyObject *self, PyObject *ar
         return NULL;
     }
     dap_hash_fast_t l_spender_hash = {0};
-    if (dap_ledger_tx_hash_is_used_out_item(((PyDapChainLedgerObject*)self)->ledger, ((PyDapHashFastObject*)tx_hash)->hash_fast, idx, &l_spender_hash){
+    if (dap_ledger_tx_hash_is_used_out_item(((PyDapChainLedgerObject*)self)->ledger, ((PyDapHashFastObject*)tx_hash)->hash_fast, idx, &l_spender_hash)){
         PyDapHashFastObject *obj_hf = PyObject_New(PyDapHashFastObject, &DapHashFastObjectType);
         obj_hf->hash_fast = DAP_NEW(dap_hash_fast_t);
         memcpy(obj_hf->hash_fast, &l_spender_hash, sizeof(dap_hash_fast_t));
