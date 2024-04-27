e84c9444196c46dbf42e237bf6cc82948669df6b
alexey.stratulat <alexey.stratulat@demlabs.net>
2024-04-27 21:50:52 +0700

    [*] Fixed getting a hash for a transaction.

--- modules/cellframe-sdk/common/src/wrapping_dap_chain_tx_out.c
+++ modules/cellframe-sdk/common/src/wrapping_dap_chain_tx_out.c
@@ -34,7 +34,7 @@ PyObject *wrapping_dap_chain_tx_out_get_used_by(PyObject *self, void *closure){
     dap_hash_fast_t l_spender_hash = {0};
     PyDapChainTXOutObject *l_obj = (PyDapChainTXOutObject*)self;
     if (dap_ledger_tx_hash_is_used_out_item(l_obj->ledger, l_obj->tx_hash, l_obj->idx, &l_spender_hash)) {
-        PyDapHashFastObject *l_hf = PyObject_New(PyDapHashFastObject, &DapHashFastObjectType);
+        PyDapHashFastObject *l_hf = PyObject_New(PyDapHashFastObject, &DapChainHashFastObjectType);
         l_hf->hash_fast = DAP_NEW(dap_hash_fast_t);
         memcpy(l_hf->hash_fast, &l_spender_hash, sizeof(dap_hash_fast_t));
         l_hf->origin = true;
