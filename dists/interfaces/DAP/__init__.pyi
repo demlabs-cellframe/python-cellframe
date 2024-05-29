f851f49ad0ca1498a97b5cc708cdc1fd35367359
alexey.stratulat <alexey.stratulat@demlabs.net>
2024-05-29 11:55:06 +0000

    Hotfix 11460

--- dists/interfaces/DAP/__init__.pyi
+++ dists/interfaces/DAP/__init__.pyi
@@ -1,13 +1,9 @@
-from typing import Protocol
+from typing import Protocol, Any
 
 
 # PyModule_AddObject(dapModule, "error", PyErr_NewException("DAP.error", NULL, NULL));
 
-def configGetItem(section_path: str, item_name: str) -> str:
+def configGetItem(section_path: str, item_name: str, default_value : Any = None) -> str | bool | int | list[str]:
     """Get an item from a configuration"""
     pass
 
-
-def configGetItemDefault(section_path: str, item_name: str, default_value: str) -> str:
-    """Get an item from a configuration with a default value"""
-    pass
