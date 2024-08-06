0ff4e66472e7401b66461068780ff0e4286cad51
roman.khlopkov <roman.khlopkov@demlabs.net>
2024-08-06 20:28:45 +0300

    [*] Chain address to string conversion fix

--- modules/cellframe-sdk/common/src/wrapping_dap_chain_common.c
+++ modules/cellframe-sdk/common/src/wrapping_dap_chain_common.c
@@ -92,9 +92,8 @@ PyObject *dap_chain_addr_to_str_py(PyObject *self, PyObject *args){
     if (!PyArg_ParseTuple(args, "O", &obj_chain_addr))
         return NULL;
     const dap_chain_addr_t *addr = ((PyDapChainAddrObject*)obj_chain_addr)->addr;
-    const char *res = dap_chain_addr_to_str(addr);
+    const char *res = dap_chain_addr_to_str_static(addr);
     PyObject *l_obj_res =  Py_BuildValue("s", res);
-    DAP_DELETE(res);
     return l_obj_res;
 }
 
@@ -155,7 +154,7 @@ PyObject *dap_chain_addr_check_sum_py(PyObject *self, PyObject *args){
 }
 
 PyObject *obj_addr_str(PyObject *self){
-    const char *l_addr = dap_chain_addr_to_str(((PyDapChainAddrObject*)self)->addr);
+    const char *l_addr = dap_chain_addr_to_str_static(((PyDapChainAddrObject*)self)->addr);
     PyObject* l_obj_res = Py_BuildValue("s", l_addr);
     return l_obj_res;
 }
