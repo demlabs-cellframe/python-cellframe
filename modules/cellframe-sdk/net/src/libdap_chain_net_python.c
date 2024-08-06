304f4e6b66ed80c4cf13309771c38c4007a6da42
alexey.stratulat <alexey.stratulat@demlabs.net>
2024-08-06 22:28:20 +0700

    ...

--- modules/cellframe-sdk/net/src/libdap_chain_net_python.c
+++ modules/cellframe-sdk/net/src/libdap_chain_net_python.c
@@ -178,8 +178,6 @@ PyObject *dap_chain_net_get_chain_by_name_py(PyObject *self, PyObject *args){
 PyObject *dap_chain_net_python_get_id(PyObject *self, void *closure){
     (void)closure;
     PyDapChainNetIdObject *obj_net_id = PyObject_New(PyDapChainNetIdObject, &DapChainNetIdObjectType);
-    PyObject_Dir((PyObject*)obj_net_id);
-    obj_net_id->net_id = ((PyDapChainNetObject*)self)->chain_net->pub.id;
     return (PyObject*)obj_net_id;
 }
 PyObject *dap_chain_net_python_get_chains(PyObject *self, void *closure){
