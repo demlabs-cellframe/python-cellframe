e89655c80a397e232ef2fa49f5f830be8d046042
alexey.stratulat <alexey.stratulat@demlabs.net>
2024-08-12 23:30:39 +0700

    [*] Added information to the interface file that the addItem method for DatumTX accepts TxIn, TxInCond, TxOut, TxOutExt, TxOutCondSubtypeSrvPay, TxOutCondSubtypeSrvStakeLock, TxOutCondSubtypeSrvStakePosDelegate, TxOutCondSubtypeSrvXchange, TxToken, TxReceipt. And these items now have a constructor.

--- dists/interfaces/CellFrame/Common.pyi
+++ dists/interfaces/CellFrame/Common.pyi
@@ -1,9 +1,11 @@
 from typing import Protocol, Any
+from xmlrpc.client import DateTime
+
 from Network import Net
-from DAP.Crypto import HashFast
-from DAP.Core import Math
-from Chain import ChainAddr
-from Network import ServiceUID
+from DAP.Crypto import HashFast, PKey
+from DAP.Core import Math, NodeAddr
+from Chain import ChainAddr, ChainID
+from Network import ServiceUID, ServicePriceUnitUID
 from Network import NetID
 from datetime import datetime
 
@@ -138,8 +140,9 @@ class DatumTx(Protocol):
     def getSize(self, args):
         pass
 
-    def addItem(self, args):
-        pass
+    def addItem(self, item : TxIn | TxInCond | TxOut | TxOutExt | TxToken | TxOutCondSubtypeSrvPay |
+                             TxOutCondSubtypeSrvStakePosDelegate | TxOutCondSubtypeSrvStakeLock |
+                             TxOutCondSubtypeSrvXchange) -> None:...
 
     def addInItem(self, args):
         pass
@@ -176,6 +179,7 @@ class TxOutCond(Protocol):
 
 # DapChainTxOutCondSubTypeSrvPayObjectType
 class TxOutCondSubtypeSrvPay(Protocol):
+    def __init__(self, value : Math, srvUID : ServiceUID, pubKey : PKey, maxPrice : Math, units : ServicePriceUnitUID, params : bytes | None = None) -> None: ...
     @property
     def unit(self) -> int: ...
     @property
@@ -190,6 +194,7 @@ class TxOutCondSubtypeSrvPay(Protocol):
 
 # DapChainTxOutCondSubTypeSrvStakeLockObjectType
 class TxOutCondSubtypeSrvStakeLock(Protocol):
+    def __init__(self, srvUID : ServiceUID, value : Math, timeStaking : DateTime, reinvestParcent : Math) -> None: ...
     @property
     def timeUnlock(self) -> datetime:...
     @property
@@ -203,6 +208,7 @@ class TxOutCondSubtypeSrvStakeLock(Protocol):
 
 # DapChainTxOutCondSubTypeSrvStakePosDelegateObjectType
 class TxOutCondSubtypeSrvStakePosDelegate(Protocol):
+    def __init__(self, srvUID : ServiceUID, value : Math, signingAddr : ChainAddr, signerNodeAddr : NodeAddr, sovereignAddr : ChainAddr, sovereignTax : Math) -> None:...
     @property
     def uid(self) -> ServiceUID: ...
     @property
@@ -215,6 +221,7 @@ class TxOutCondSubtypeSrvStakePosDelegate(Protocol):
 
 # DapChainTxOutCondSubTypeSrvXchangeObjectType
 class TxOutCondSubtypeSrvXchange(Protocol):
+    def __init__(self, UID : ServiceUID, sellNetID : NetID, valueSell : Math, buyNetId : NetID, token : str, valueRate : Math, sellerAddr : ChainAddr) -> None: ...
     @property
     def uid(self) -> ServiceUID: ...
     @property
@@ -229,16 +236,27 @@ class TxOutCondSubtypeSrvXchange(Protocol):
 
 # DapChainTxInObjectType
 class TxIn(Protocol):
-    pass
+    def __init__(self, prevHash : HashFast, prevIdx : int) -> None: ...
+    @property
+    def prevHash(self) -> HashFast: ...
+    @property
+    def prevIdx(self) -> int: ...
 
 
 # DapChainTxInCondObjectType
 class TxInCond(Protocol):
-    pass
+    def __init__(self, prevHash : HashFast, outPrevIdx : int, receiptIdx : int) -> None: ...
+    @property
+    def receiptPrevIdx(self) -> int: ...
+    @property
+    def prevHash(self) -> HashFast: ...
+    @property
+    def outPrevIdx(self) -> int: ...
 
 
 # DapChainTxOutObjectType
 class TxOut(Protocol):
+    def __init__(self, addr : ChainAddr, value : Math) -> None: ...
     @property
     def addr(self) -> ChainAddr : ...
     @property
@@ -259,16 +277,23 @@ class TxSig(Protocol):
 
 # DapChainTxTokenObjectType
 class TxToken(Protocol):
-    pass
+    def __init__(self, chainId : ChainID, hashEmi : HashFast) -> None: ...
+    @property
+    def ticker(self) -> str: ...
+    @property
+    def tokenEmissionHash(self) -> HashFast: ...
+    @property
+    def tokenEmissionChainId(self) -> ChainID: ...
 
 
 # DapChainTxReceiptObjectType
 class TxReceipt(Protocol):
-    pass
+    def __init__(self, srv_uid : ServiceUID, units_type : ServicePriceUnitUID, units : int, value : Math, ext : bytes | None = None) -> None: ...
 
 
 # DapChainTxOutExtObjectType
 class TxOutExt(Protocol):
+    def __init__(self, addr : ChainAddr, token : str, value : Math) -> None: ...
     @property
     def addr(self) -> ChainAddr : ...
     @property
