2134caefffc9cad9b0d920faa6ab86c58bed493c
pavel.uhanov <pavel.uhanov@demlabs.net>
2024-11-12 12:22:51 +0300

    [*] remove start callback from dap_chain_cs_add

--- modules/cellframe-sdk/chain/src/wrapping_dap_chain_cs.c
+++ modules/cellframe-sdk/chain/src/wrapping_dap_chain_cs.c
@@ -65,7 +65,7 @@ PyObject *dap_chain_cs_add_py (PyObject *self, PyObject *args){
     Py_XINCREF(obj_callback_init);
     Py_XDECREF(binded_object_callback_new_cfg);
     binded_object_callback_new_cfg = obj_callback_init;
-    dap_chain_cs_add(cs_str, ((PyDapChainCsObject*)self)->callback_new_cfg, ((PyDapChainCsObject*)self)->callback_start_cfg);
+    dap_chain_cs_add(cs_str, ((PyDapChainCsObject*)self)->callback_new_cfg);
     return PyLong_FromLong(0);
 }
 PyObject *dap_chain_cs_create_py(PyObject *self, PyObject *args){
