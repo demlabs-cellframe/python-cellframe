48160b336a1a72fa280100d7596a62dc2b6ad6c3
Constantin P. <papizh.konstantin@demlabs.net>
2025-03-21 16:12:14 +0700

    log fix

--- include/python-cellframe_common.h
+++ include/python-cellframe_common.h
@@ -8,7 +8,7 @@
 #include "dap_strfuncs.h"
 
 void python_error_in_log_it(const char *a_tag);
-void _PyErr_logIt(const dap_log_level_t a_level, const char *a_tag, const char *a_msg);
+#define _PyErr_logIt(a_level, a_tag, ...) _log_it(NULL, 0, a_tag, a_level, ##__VA_ARGS__)
 PyObject *python_get_config_item(dap_config_t* a_config, const char *a_section, const char *a_key, PyObject *obj_default);
 
 DAP_STATIC_INLINE uint64_t PyDateTime_to_timestamp_uint64(PyObject *obj_date_time) {
