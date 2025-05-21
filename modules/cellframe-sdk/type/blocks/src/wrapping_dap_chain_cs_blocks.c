883725ea4f08c7556f77d2f783c62feabd1e14d0
daniil.frolov <daniil.frolov@demlabs.net>
2025-05-21 18:21:08 +0700

    [+] Added ChainCSBlockRewards wrappers

--- modules/cellframe-sdk/type/blocks/src/wrapping_dap_chain_cs_blocks.c
+++ modules/cellframe-sdk/type/blocks/src/wrapping_dap_chain_cs_blocks.c
@@ -1,16 +1,22 @@
 #include "libdap-python.h"
 #include "wrapping_dap_chain_cs_blocks.h"
+#include "python-cellframe_common.h"
+#include "wrapping_dap_hash.h"
 
 static PyMethodDef DapChainCsBlocksMethod[]  = {
         {"getBlockByhHash", (PyCFunction)wrapping_dap_chain_cs_block_get_block, METH_VARARGS | METH_STATIC, ""},
         {"getBlocks", (PyCFunction)wrapping_dap_chain_cs_block_get_list_blocks, METH_VARARGS | METH_STATIC, ""},
-        {}
+        {"getBlockSignersRewards", (PyCFunction)wrapping_dap_chain_cs_block_get_block_signers_rewards, METH_VARARGS | METH_STATIC, ""},
 };
 
 PyTypeObject DapChainCsBlocksType = DAP_PY_TYPE_OBJECT(
         "CellFrame.Consensus.ChainCSBlocks", sizeof(PyDapChainCSBlocksObject),
         "Chain cs blocks objects",
         .tp_methods = DapChainCsBlocksMethod);
+
+PyTypeObject DapChainCSBlockRewardsType = DAP_PY_TYPE_OBJECT(
+        "CellFrame.Consensus.ChainCSBlockRewards", sizeof(PyDapChainCSBlockRewardsObject),
+        "Chain cs block rewards objects");
 
 PyObject *wrapping_dap_chain_cs_block_get_block(PyObject *self, PyObject *args){
     //TODO: Get a block by its hash.
@@ -37,4 +43,38 @@ PyObject *wrapping_dap_chain_cs_block_get_list_blocks(PyObject *self, PyObject *
 
 //    l_blocks->
     Py_RETURN_NONE;
+}
+
+PyObject *wrapping_dap_chain_cs_block_get_block_signers_rewards(PyObject *self, PyObject *args){
+    
+    PyDapHashFastObject *obj_block_hash = NULL;
+
+    if (!PyArg_ParseTuple(args, "0", &obj_block_hash)){
+        return NULL;
+    }
+    if (!PyDapHashFast_Check(obj_block_hash)) {
+        PyErr_SetString(PyExc_Exception, "The first argument must be an instance of the DAP.HashFast object");
+        return NULL;
+    }
+
+    dap_hash_fast_t l_block_hash = *(obj_block_hash->hash_fast);
+
+    dap_list_t *l_rewards_list = dap_chain_cs_blocks_get_block_signers_rewards(((PyDapChainCSBlocksObject*)self)->blocks->chain, &l_block_hash);
+
+    size_t l_rewards_list_count = dap_list_length(l_rewards_list);
+    dap_list_t *tmp = l_rewards_list;
+    PyObject *obj_rewards_list = PyList_New((Py_ssize_t)l_rewards_list_count);
+    for (size_t i = 0; i < l_rewards_list_count; i++){
+        dap_chain_cs_block_rewards_t *l_reward = (dap_chain_cs_block_rewards_t*)tmp->data;
+        PyDapChainCSBlockRewardsObject *l_obj_reward = PyObject_New(PyDapChainCSBlockRewardsObject,
+                                                                       &DapChainCSBlockRewardsType);
+        l_obj_reward->rewards = DAP_NEW(dap_chain_cs_block_rewards_t);
+        memcpy(l_obj_reward->rewards, l_reward, sizeof(dap_chain_cs_block_rewards_t));
+        PyList_Append(obj_rewards_list, (PyObject*)l_obj_reward);
+        Py_XDECREF(l_obj_reward);
+        tmp = tmp->next;
+    }
+    dap_list_free(l_rewards_list);
+    return obj_rewards_list;
+
 }
