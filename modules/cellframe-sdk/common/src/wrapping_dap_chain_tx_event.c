a595a2b75b7426313ee53c3e789a1cac59d0d1d3
roman.khlopkov <roman.khlopkov@demlabs.net>
2025-09-18 12:22:45 +0300

    [*] Auctions debug

--- modules/cellframe-sdk/common/src/wrapping_dap_chain_tx_event.c
+++ modules/cellframe-sdk/common/src/wrapping_dap_chain_tx_event.c
@@ -1,5 +1,6 @@
 #include "wrapping_dap_chain_tx_event.h"
 #include "dap_chain_datum_tx_event.h"
+#include "dap_chain_net_srv_auctions.h"
 
 #define LOG_TAG "wrapping_dap_chain_tx_event"
 
@@ -13,73 +14,11 @@ static PyGetSetDef DapChainTxEventGetsSetsDef[] = {
     {}
 };
 
-static PyObject *TX_EVENT_DATA_AUCTION_STARTED_CREATE_PY(PyObject *self, PyObject *args)
-{
-    (void)self;
-    unsigned int multiplier;
-    unsigned long long duration; // dap_time_t
-    unsigned int time_unit; // dap_chain_tx_event_data_time_unit_t
-    unsigned int calculation_rule_id;
-    PyObject *py_projects = NULL;
-    if (!PyArg_ParseTuple(args, "IKII|O", &multiplier, &duration, &time_unit, &calculation_rule_id, &py_projects))
-        return NULL;
-
-    uint8_t projects_cnt = 0;
-    uint32_t *project_ids = NULL;
-
-    if (py_projects && py_projects != Py_None) {
-        if (!PyList_Check(py_projects) && !PyTuple_Check(py_projects)) {
-            PyErr_SetString(PyExc_TypeError, "project_ids must be a list or tuple of integers");
-            return NULL;
-        }
-        Py_ssize_t n = PySequence_Size(py_projects);
-        if (n < 0)
-            return NULL;
-        if (n > 255) {
-            PyErr_SetString(PyExc_ValueError, "project_ids length must be <= 255");
-            return NULL;
-        }
-        projects_cnt = (uint8_t)n;
-        if (projects_cnt) {
-            project_ids = (uint32_t *)DAP_NEW_Z_COUNT(uint32_t, projects_cnt);
-            if (!project_ids) {
-                PyErr_SetString(PyExc_MemoryError, "Allocation failed");
-                return NULL;
-            }
-            for (Py_ssize_t i = 0; i < n; i++) {
-                PyObject *item = PySequence_GetItem(py_projects, i);
-                if (!item) { DAP_DELETE(project_ids); return NULL; }
-                unsigned long val = PyLong_AsUnsignedLong(item);
-                Py_DECREF(item);
-                if (PyErr_Occurred()) { DAP_DELETE(project_ids); return NULL; }
-                project_ids[i] = (uint32_t)val;
-            }
-        }
-    }
-
-    size_t data_size = 0;
-    byte_t *data = dap_chain_tx_event_data_auction_started_create(
-        &data_size, (uint32_t)multiplier, (dap_time_t)duration, (dap_chain_tx_event_data_time_unit_t)time_unit,
-        (uint32_t)calculation_rule_id, projects_cnt, (uint32_t *)project_ids);
-
-    if (project_ids)
-        DAP_DELETE(project_ids);
-
-    if (!data) {
-        Py_RETURN_NONE;
-    }
-
-    PyObject *py_bytes = PyBytes_FromStringAndSize((const char *)data, (Py_ssize_t)data_size);
-    DAP_DELETE(data);
-    return py_bytes;
-}
-
 static PyMethodDef PyDapChainTxEventObjectMethods[] ={
     {"AUCTION_STARTED", (PyCFunction)TX_EVENT_TYPE_AUCTION_STARTED_PY, METH_NOARGS | METH_STATIC, ""},
     {"AUCTION_BID_PLACED", (PyCFunction)TX_EVENT_TYPE_AUCTION_BID_PLACED_PY, METH_NOARGS | METH_STATIC, ""},
     {"AUCTION_ENDED", (PyCFunction)TX_EVENT_TYPE_AUCTION_ENDED_PY, METH_NOARGS | METH_STATIC, ""},
     {"AUCTION_CANCELLED", (PyCFunction)TX_EVENT_TYPE_AUCTION_CANCELLED_PY, METH_NOARGS | METH_STATIC, ""},
-    {"auction_started_create", (PyCFunction)TX_EVENT_DATA_AUCTION_STARTED_CREATE_PY, METH_VARARGS | METH_STATIC, "Create auction_started event data bytes"},
     {}
 };
 
