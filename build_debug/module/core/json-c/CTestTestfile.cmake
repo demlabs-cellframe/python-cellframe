de4defb156cfe6c5b705b04b84c31f9044bf3716
naeper <ceo@demlabs.net>
2025-10-21 13:20:07 +0700

    [-] Garbage cleanup

--- build_debug/module/core/json-c/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/naeper/work/python-cellframe/dap-sdk/3rdparty/json-c
-# Build directory: /home/naeper/work/python-cellframe/build_debug/module/core/json-c
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build_debug/module/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/naeper/work/python-cellframe/dap-sdk/module/test
-# Build directory: /home/naeper/work/python-cellframe/build_debug/module/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
